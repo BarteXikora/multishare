e2e6ab760f7afe7fc9f253736b7869aac88680e0
BarteXikora <sbartek50@gmail.com>
2023-12-04 20:49:06 +0100

    Created GlobalStyle component and placed it in AppTheme component.

---
+++ src/theme/GlobalStyles.tsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    html, body, #root {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        font-size: ${(props) => props.theme.fontSizes.default};
+        background-color: ${(props) => props.theme.colors.white};
+        color: ${(props) => props.theme.colors.black};
+    }
+`
+
+export default GlobalStyle
