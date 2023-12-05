6c925e51ac36cb835afa281f61ac3a5a9617b9d7
BarteXikora <sbartek50@gmail.com>
2023-12-05 12:30:34 +0100

    Created Button component with styles, variants, etc.

---
+++ src/components/ui/button/Button.tsx
@@ -0,0 +1,9 @@
+import StyledButton from './Button.styles'
+
+export type ButtonProps = {
+    variant?: 'primary' | 'secondary' | 'tertiary'
+    size?: 'big' | 'small'
+    active?: boolean
+}
+
+export default StyledButton
