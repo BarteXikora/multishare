4fbf7ca2d51ccd201b8c045bf44df8520ad7db36
BarteXikora <sbartek50@gmail.com>
2023-12-05 13:32:04 +0100

    Added logo to the SideMenu component.

--- src/components/layout/sideMenu/SideMenu.tsx
+++ src/components/layout/sideMenu/SideMenu.tsx
@@ -1,8 +1,14 @@
 import StyledSideMenu from './SideMenu.styles'
+import Button from '../../ui/button/Button'
+import logo from '../../../assets/images/img-logo.svg'
 
 const SideMenu = () => {
     return <StyledSideMenu>
-        sidemenu
+        <section>
+            <Button variant='tertiary'>
+                <img src={logo} alt="Logo multishare" />
+            </Button>
+        </section>
     </StyledSideMenu>
 }
 
