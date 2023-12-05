35bc136f695464da76816264b1c0a762dbb70a8a
BarteXikora <sbartek50@gmail.com>
2023-12-05 15:08:23 +0100

    Created UsedSpace component showing current cloud space usage.

---
+++ src/components/elements/usedSpace/UsedSpace.styles.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+const StyledUsedSpace = styled.div`
+    button {
+        flex-direction: column;
+        align-items: start;
+    }
+
+    h2 {
+        margin: 0;
+        font-size: ${(props) => props.theme.fontSizes.default};
+        font-weight: normal;
+    }
+
+    .progress-bar {
+        height: 12px;
+        width: 100%;
+        background-color: ${(props) => props.theme.colors.gray2};
+        margin: ${(props) => props.theme.margins.small} 0;
+        border-radius: ${(props) => props.theme.borderRadiuses.big};
+
+        .progress {
+            height: 100%;
+            max-width: 100%;
+            background-color: ${(props) => props.theme.colors.primary6};
+            border-radius: ${(props) => props.theme.borderRadiuses.big};
+        }
+    }
+
+    span {
+        font-weight: bold;
+    }
+`
+
+export default StyledUsedSpace
