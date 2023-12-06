2ad0114338f00419402125d0cb193a85283db876
BarteXikora <sbartek50@gmail.com>
2023-12-06 17:46:45 +0100

    Placed the ContentTools component in the Header component.

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,10 +1,15 @@
 import StyledHeader from './Header.styles'
 import PathBox from '../../elements/pathBox/PathBox'
+import ContentTools from '../../elements/contentTools/ContentTools'
 
 const Header = () => {
     return <StyledHeader>
         <section className='path-box'>
             <PathBox />
+        </section>
+
+        <section className='content-tools'>
+            <ContentTools />
         </section>
     </StyledHeader>
 }
