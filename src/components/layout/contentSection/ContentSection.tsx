5971043151c5b20d1229925e5587be6de659f1d5
BarteXikora <sbartek50@gmail.com>
2023-12-07 11:38:38 +0100

    Added hardcoded files to the ContentSection component.

--- src/components/layout/contentSection/ContentSection.tsx
+++ src/components/layout/contentSection/ContentSection.tsx
@@ -1,10 +1,15 @@
 import StyledContentSection from './ContentSection.styles'
 import Folder from '../../elements/folder/Folder'
+import File from '../../elements/file/File'
 
 const __currentContent = {
     folders: [
         { id: 123456, displayName: 'Pozostałe' },
         { id: 123457, displayName: 'Nieobrobione - głównie do kosza', star: true }
+    ],
+    files: [
+        { id: 98765, displayName: 'Wycieczka na rowery 1', extension: 'PNG' },
+        { id: 98766, displayName: 'Wycieczka na rowery 2', extension: 'JPG', star: true }
     ]
 }
 
@@ -28,6 +33,19 @@ const ContentSection = () => {
 
         <section className='files-section'>
             <h2>Pliki:</h2>
+
+            <div className="content">
+                {
+                    __currentContent.files.map(file => {
+                        return <File
+                            key={file.id}
+                            displayName={file.displayName}
+                            extension={file.extension}
+                            isStar={file.star || false}
+                        />
+                    })
+                }
+            </div>
         </section>
     </StyledContentSection>
 }
