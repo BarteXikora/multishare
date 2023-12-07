3c5d2d0689448a933aaf8b375867096225e0991c
BarteXikora <sbartek50@gmail.com>
2023-12-07 18:58:52 +0100

    Placed the DropSection component in the DetailsSection component.

--- src/components/layout/detailsSection/DetailsSection.tsx
+++ src/components/layout/detailsSection/DetailsSection.tsx
@@ -1,9 +1,13 @@
 import StyledDetailsSection from './DetailsSection.styles'
+
+import DropSection from '../../elements/dropSection/DropSection'
 import ProcessPill from '../../elements/processPill/ProcessPill'
 
 const DetailsSection = () => {
     return <StyledDetailsSection>
-        <section className="main-content"></section>
+        <section className="main-content">
+            <DropSection />
+        </section>
 
         <ProcessPill />
     </StyledDetailsSection>
