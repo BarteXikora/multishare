8500375c9ddf17be04d012ecab179c502219e383
BarteXikora <sbartek50@gmail.com>
2023-12-07 18:15:06 +0100

    Placed the DetailsSection component in the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import TopBar from './components/layout/topBar/TopBar'
 import BodyColumns from './components/layout/bodyColumns/BodyColumns'
 import Header from './components/layout/header/Header'
 import ContentSection from './components/layout/contentSection/ContentSection'
+import DetailsSection from './components/layout/detailsSection/DetailsSection'
 
 const App = () => {
   return <AppTheme>
@@ -21,7 +22,9 @@ const App = () => {
           <ContentSection />
         </section>
 
-        <aside className='aside-column'></aside>
+        <aside className='aside-column'>
+          <DetailsSection />
+        </aside>
       </BodyColumns>
     </MainBody>
   </AppTheme>
