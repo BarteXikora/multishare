0ee2e0427e3aed847595f1f0a4eaae2e1080bd92
BarteXikora <sbartek50@gmail.com>
2023-12-08 14:10:51 +0100

    Removed the DetailsSection component from the App component for the dev purposes.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import TopBar from './components/layout/topBar/TopBar'
 import BodyColumns from './components/layout/bodyColumns/BodyColumns'
 import Header from './components/layout/header/Header'
 import ContentSection from './components/layout/contentSection/ContentSection'
-import DetailsSection from './components/layout/detailsSection/DetailsSection'
+// import DetailsSection from './components/layout/detailsSection/DetailsSection'
 
 import ScreenSize from './components/__dev/__ScreenSize'
 
@@ -29,9 +29,9 @@ const App = () => {
           <ContentSection />
         </section>
 
-        <aside className='aside-column'>
+        {/* <aside className='aside-column'>
           <DetailsSection />
-        </aside>
+        </aside> */}
       </BodyColumns>
     </MainBody>
   </AppTheme>
