158ebe1dbae2e505f5f6cb9b369f331d5c22f0de
BarteXikora <sbartek50@gmail.com>
2023-12-08 14:29:30 +0100

    Fixed top offset of the DetailsSection component.

--- src/components/layout/detailsSection/DetailsSection.styles.tsx
+++ src/components/layout/detailsSection/DetailsSection.styles.tsx
@@ -4,8 +4,8 @@ const StyledDetailsSection = styled.div`
     display: flex;
     flex-direction: column;
     position: sticky;
-    top: 92px;
-    height: calc(100vh - 92px - ${(props) => props.theme.margins.big});
+    top: 98px;
+    height: calc(100vh - 98px - ${(props) => props.theme.margins.big});
     box-sizing: border-box;
     background-color: ${(props) => props.theme.colors.gray2};
     border-radius: ${(props) => props.theme.borderRadiuses.big};
