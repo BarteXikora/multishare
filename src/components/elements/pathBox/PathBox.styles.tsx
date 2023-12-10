43512c93a9bfebc70e2aec17501393cbc224098e
BarteXikora <sbartek50@gmail.com>
2023-12-10 11:12:11 +0100

    Created the PathBox component collapse behavior.

--- src/components/elements/pathBox/PathBox.styles.tsx
+++ src/components/elements/pathBox/PathBox.styles.tsx
@@ -8,9 +8,26 @@ const StyledPathBox = styled.nav`
     padding: ${(props) => props.theme.margins.sectionMedium};
     border-radius: ${(props) => props.theme.borderRadiuses.big};
 
+    .collapsed-path-separator {
+        display: flex;
+        align-items: center;
+        font-size: ${(props) => props.theme.fontSizes.subtitle};
+        font-weight: bold;
+        padding-left: ${(props) => props.theme.margins.medium};
+    }
+
+    .path {
+        display: flex;
+        gap: ${(props) => props.theme.margins.small};
+    }
+
     @media (max-width: ${(props) => props.theme.screenBreakpoints.mobile}) {
         padding: ${(props) => props.theme.margins.sectionSmall};
         gap: 0;
+
+        .path, .collapsed-path-separator {
+            display: none;
+        }
     }
 `
 
--- src/components/elements/pathBox/PathBox.tsx
+++ src/components/elements/pathBox/PathBox.tsx
@@ -1,9 +1,11 @@
 import StyledPathBox from './PathBox.styles'
 import Button from '../../ui/button/Button'
+import CollapsedPathButton from './collapsedPathButton/CollapsedPathButton'
 import PathButton from './pathButton/PathButton'
 
 import iconBack from '../../../assets/icons/icon-back.svg'
 import iconHome from '../../../assets/icons/icon-home.svg'
+import iconArrow from '../../../assets/icons/icon-arrow-right.svg'
 
 const __currentPath = ['Moje pliki', 'Prywatne', 'Obrazy', 'Wycieczka na rowery 2023']
 
@@ -19,16 +21,49 @@ const PathBox = () => {
 
         </Button>
 
+        <CollapsedPathButton
+            path={__currentPath}
+            isPathCollapsed={__currentPath.length > 4}
+        />
+
         {
-            __currentPath.map((path, n) => {
-                return <PathButton
-                    key={n}
-                    displayName={path}
-                    isLast={n === __currentPath.length - 1}
-                />
-            })
+            __currentPath.length > 4 && <div className='collapsed-path-separator'>
+                <span>...</span>
+
+                <img src={iconArrow} alt="/" />
+            </div>
         }
+
+        <div className="path">
+            {
+                __currentPath.map((path, n) => {
+                    if (__currentPath.length <= 4 || (__currentPath.length > 4 && n > __currentPath.length - 3))
+                        return <PathButton
+                            key={n}
+                            displayName={path}
+                            isLast={n === __currentPath.length - 1}
+                        />
+
+                    return ''
+                })
+            }
+        </div>
     </StyledPathBox>
 }
 
 export default PathBox
+
+
+/*
+<div className="path">
+            {
+                __currentPath.map((path, n) => {
+                    return <PathButton
+                        key={n}
+                        displayName={path}
+                        isLast={n === __currentPath.length - 1}
+                    />
+                })
+            }
+        </div>
+*/
