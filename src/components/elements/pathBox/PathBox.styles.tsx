5bfd3577ee6ffbe5f454cc5b311554840a92def0
BarteXikora <sbartek50@gmail.com>
2023-12-12 10:06:14 +0100

    Prepared the new PathBox component.

---
+++ src/components/elements/pathBox/PathBox.styles.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const StyledPathBox = styled.nav`
+    display: flex;
+    gap: ${(props) => props.theme.margins.small};
+    background-color: ${(props) => props.theme.colors.black};
+    color: ${(props) => props.theme.colors.white};
+    padding: ${(props) => props.theme.margins.sectionMedium};
+    border-radius: ${(props) => props.theme.borderRadiuses.big};
+
+    @media (max-width: ${(props) => props.theme.screenBreakpoints.mobile}) {
+        padding: ${(props) => props.theme.margins.sectionSmall};
+        gap: 0;
+    }
+`
+
+export default StyledPathBox
---
+++ src/components/elements/pathBox/PathBox.tsx
@@ -0,0 +1,9 @@
+import StyledPathBox from './PathBox.styles'
+
+const __currentPath = ['Moje pliki', 'Prywatne', 'Obrazy', 'Wycieczka na rowery 2023']
+
+const PathBox = () => {
+    return <StyledPathBox>path box</StyledPathBox>
+}
+
+export default PathBox
