f759051dd809ac106398fe8f696d2aa304013d78
BarteXikora <sbartek50@gmail.com>
2023-12-12 13:30:57 +0100

    Used the getShortenName function in the PathBox component.

--- src/components/elements/pathBox/PathBox.tsx
+++ src/components/elements/pathBox/PathBox.tsx
@@ -1,17 +1,29 @@
+import { useState, useEffect } from 'react'
 import StyledPathBox from './PathBox.styles'
 import BackButton from './backButton/BackButton'
 import Path from './path/Path'
 import CurrentFolderButton from './currentFolderButon/CurrentFolderButton'
 
-const __currentPath = ['Moje pliki', 'Prywatne', 'Obrazy', 'Wycieczka na rowery 2023']
+import getShortenName from '../../../functions/getShortenName/getShortenName'
+
+const MAX_FOLDER_NAME_LENGTH = 25
+
+const __currentPath = ['Moje pliki', 'Prywatne', 'Obrazy', 'Wycieczka na rowery 2023 - wyjazd']
 
 const PathBox = () => {
+    const [shortenPath, setShortenPath] = useState<string[]>([])
+
+    useEffect(() => {
+        setShortenPath(__currentPath.map(name => getShortenName(name, MAX_FOLDER_NAME_LENGTH)))
+
+    }, [])
+
     return <StyledPathBox>
-        <BackButton isHome={__currentPath.length <= 1} />
+        <BackButton isHome={shortenPath.length <= 1} />
 
-        <Path path={__currentPath} />
+        <Path path={shortenPath} />
 
-        <CurrentFolderButton path={__currentPath} />
+        <CurrentFolderButton path={shortenPath} />
     </StyledPathBox>
 }
 
