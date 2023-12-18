6f7af0a69bb9edcf46b228c102f3c645cbbecdc6
BarteXikora <sbartek50@gmail.com>
2023-12-18 14:19:18 +0100

    Devided Button type into separate types so only tertiary button can be active.

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,9 +1,28 @@
 import StyledButton from './Button.styles'
 
-export type ButtonProps = {
-    $variant?: 'primary' | 'secondary' | 'tertiary' | 'wrong'
-    $size?: 'big' | 'small'
+type ButtonSizes = 'big' | 'small'
+
+type ButtonPrimary = {
+    $variant?: 'primary'
+    $size?: ButtonSizes
+}
+
+type ButtonSecondary = {
+    $variant: 'secondary'
+    $size?: ButtonSizes
+}
+
+type ButtonTertiary = {
+    $variant: 'tertiary'
+    $size?: ButtonSizes
     $active?: boolean
 }
+
+type ButtonWrong = {
+    $variant: 'wrong'
+    $size?: ButtonSizes
+}
+
+export type ButtonProps = ButtonPrimary | ButtonSecondary | ButtonTertiary | ButtonWrong
 
 export default StyledButton
