8a885b804081c362bd169a5c2af3c669e6e2370c
BarteXikora <sbartek50@gmail.com>
2024-01-04 03:00:38 +0100

    Prepared the contentSlice.

---
+++ src/store/features/contentSlice/contentSlice.ts
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const contentSlice = createSlice({
+    name: 'contentSlice',
+    initialState: {},
+    reducers: {}
+})
+
+export default contentSlice.reducer
+export const { } = contentSlice.actions
