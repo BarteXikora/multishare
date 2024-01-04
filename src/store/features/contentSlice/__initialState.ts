9d8ada96f762001049342152a803b98fd654f0f5
BarteXikora <sbartek50@gmail.com>
2024-01-04 18:08:05 +0100

    Removed example data from the currentFolder in the __initialState.

--- src/store/features/contentSlice/__initialState.ts
+++ src/store/features/contentSlice/__initialState.ts
@@ -2,6 +2,7 @@ import { initialStateType } from './contentSlice.types'
 
 const __initialState: initialStateType = {
     currentPath: [],
+    currentFolder: {},
     loadedContent: {
         folders: [
             {
@@ -64,30 +65,6 @@ const __initialState: initialStateType = {
             }
         ],
         files: []
-    },
-
-    currentFolder: {
-        folders: [{
-            id: 0,
-            name: 'Test A',
-            star: false
-        }, {
-            id: 1,
-            name: 'Test B',
-            star: true
-        }],
-
-        files: [{
-            id: 0,
-            name: 'Plik 1',
-            extension: 'PNG',
-            star: true
-        }, {
-            id: 1,
-            name: 'Plik 2',
-            extension: 'TXT',
-            star: false
-        }]
     }
 }
 
