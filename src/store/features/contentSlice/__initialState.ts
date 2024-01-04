d2333f73ccd95071041f7bef07bf1e92381159bc
BarteXikora <sbartek50@gmail.com>
2024-01-04 04:38:41 +0100

    Added the loadedContent object to the initial state.

--- src/store/features/contentSlice/__initialState.ts
+++ src/store/features/contentSlice/__initialState.ts
@@ -1,4 +1,62 @@
 const __initialState = {
+    loadedContent: {
+        folders: [
+            {
+                id: 0,
+                name: 'Prywatne',
+                star: true,
+                content: {
+                    folders: [
+                        {
+                            id: 3,
+                            name: 'Obrazy',
+                            content: {
+                                folders: [
+                                    {
+                                        id: 5,
+                                        name: 'Wycieczka na rowery - sierpień 2023',
+                                        content: {
+                                            folders: [
+                                                {
+                                                    id: 6,
+                                                    name: 'Nieobrobione',
+                                                    star: true,
+                                                    content: {}
+                                                }, {
+                                                    id: 7,
+                                                    name: 'Pozostałe - większość do śmieci',
+                                                    content: {}
+                                                }
+                                            ],
+                                            files: [
+                                                {
+                                                    id: 100,
+                                                    name: 'Wycieczka na rowery 1',
+                                                    extension: 'PNG'
+                                                }
+                                            ]
+                                        }
+                                    }
+                                ],
+                                files: []
+                            }
+                        }, {
+                            id: 4,
+                            name: 'Dokumenty',
+                            content: {}
+                        }
+                    ],
+                    files: []
+                }
+            }, {
+                id: 1,
+                name: 'Pozostałe',
+                content: {}
+            }
+        ],
+        files: []
+    },
+
     currentFolder: {
         folders: [{
             id: 0,
