6330fdd1752791e8d14edb425c4bd366c1a0759a
BarteXikora <sbartek50@gmail.com>
2024-01-05 19:39:46 +0100

    Used the useGoBack hook in the EmptyFolder component.

--- src/components/elements/emptyFolder/EmptyFolder.tsx
+++ src/components/elements/emptyFolder/EmptyFolder.tsx
@@ -1,11 +1,15 @@
 import StyledEmptyFolder from './EmptyFolder.styles'
 import Button from '../../ui/button/Button'
 
+import useGoBack from '../../../functions/useGoBack/useGoBack'
+
 import imageEmptyFolder from '../../../assets/images/img-empty-folder.svg'
 import iconBack from '../../../assets/icons/icon-back.svg'
 import iconUpload from '../../../assets/icons/icon-upload.svg'
 
 const EmptyFolder = () => {
+    const goBack = useGoBack()
+
     return <StyledEmptyFolder>
         <div className="image">
             <img src={imageEmptyFolder} alt="" />
@@ -17,7 +21,7 @@ const EmptyFolder = () => {
             <p>Możesz przesłać pliki na dysk poprzez przeciągnięcie i opuszczenie je na okno przeglądarki.</p>
 
             <div className="actions">
-                <Button $variant='secondary' className='back-button'>
+                <Button $variant='secondary' className='back-button' onClick={() => goBack()}>
                     <img src={iconBack} alt='Cofnij się' />
                 </Button>
 
