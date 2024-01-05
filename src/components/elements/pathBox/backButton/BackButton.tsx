7918e465ea4fc955b6201bda05295289047f2e17
BarteXikora <sbartek50@gmail.com>
2024-01-05 17:48:08 +0100

    Added functionality to the BackButton component.

--- src/components/elements/pathBox/backButton/BackButton.tsx
+++ src/components/elements/pathBox/backButton/BackButton.tsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from '../../../../store/store'
+import { setTreeLocation } from '../../../../store/features/contentSlice/contentSlice'
+
 import StyledBackButton from './BackButton.styles'
 
 import iconBack from '../../../../assets/icons/icon-back.svg'
 import iconHome from '../../../../assets/icons/icon-home.svg'
 
 const BackButton = ({ isHome }: { isHome: boolean }) => {
-    return <StyledBackButton $variant='tertiary'>
+    const currentPath = useSelector(state => state.content.currentPath)
+    const dispatch = useDispatch()
+
+    const [previousID, setPreviousID] = useState<number>(-1)
+
+    useEffect(() => {
+        setPreviousID(currentPath.length > 1 ? currentPath[currentPath.length - 2].id : -1)
+
+    }, [currentPath])
+
+    return <StyledBackButton $variant='tertiary' onClick={() => !isHome ? dispatch(setTreeLocation(previousID)) : null}>
         {
             isHome ?
                 <img src={iconHome} alt="Folder główny" />
