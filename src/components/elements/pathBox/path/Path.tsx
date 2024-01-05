ad867d04223be35f15db488fe76d7d40810bf5de
BarteXikora <sbartek50@gmail.com>
2024-01-05 18:28:15 +0100

    Added functionality to buttons in the Path component.

--- src/components/elements/pathBox/path/Path.tsx
+++ src/components/elements/pathBox/path/Path.tsx
@@ -1,3 +1,6 @@
+import { useDispatch } from '../../../../store/store'
+import { setTreeLocation } from '../../../../store/features/contentSlice/contentSlice'
+
 import StyledPath from './Path.styles'
 import CollapsedPathButton from '../collapsedPathButton/CollapsedPathButton'
 import Button from '../../../ui/button/Button'
@@ -7,6 +10,8 @@ import { pathType } from '../../../../store/features/contentSlice/contentSlice.t
 import iconArrow from '../../../../assets/icons/icon-arrow-right.svg'
 
 const Path = ({ path }: { path: pathType }) => {
+    const dispatch = useDispatch()
+
     return <StyledPath>
         {path.length > 4 && <CollapsedPathButton path={path} />}
 
@@ -14,7 +19,14 @@ const Path = ({ path }: { path: pathType }) => {
             path.map((pathElement, n) => {
                 if (path.length <= 4 || (path.length > 4 && n > path.length - 3))
                     return <>
-                        <Button key={n} $variant='tertiary' className='path-button'>{pathElement.name}</Button>
+                        <Button
+                            key={n}
+                            $variant='tertiary'
+                            className='path-button'
+                            onClick={() => dispatch(setTreeLocation(pathElement.id))}
+                        >
+                            {pathElement.name}
+                        </Button>
 
                         {n < path.length - 1 && <img src={iconArrow} alt="/" />}
                     </>
