c0cb40936794d5a0310a1f3887a62919dc660e89
BarteXikora <sbartek50@gmail.com>
2024-01-05 19:28:04 +0100

    Created the EmptyFolder component.

---
+++ src/components/elements/emptyFolder/EmptyFolder.styles.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+
+const StyledEmptyFolder = styled.div`
+    display: inline-flex;
+    align-items: center;
+    gap: ${(props) => props.theme.margins.extraBig};
+    background-color: ${(props) => props.theme.colors.gray2};
+    padding: ${(props) => props.theme.margins.sectionBig};
+    border-radius: ${(props) => props.theme.borderRadiuses.big};
+
+    p {
+        margin: ${(props) => `0 0 ${props.theme.margins.big} 0`}
+    }
+
+    .back-button {
+        background-color: ${(props) => props.theme.colors.gray6};
+
+        &:hover {
+            background-color: ${(props) => props.theme.colors.black};
+        }
+    }
+
+    .actions {
+        display: flex;
+        gap: ${(props) => props.theme.margins.medium};
+    }
+
+    @media (max-width: ${(props) => props.theme.screenBreakpoints.smallerMobile}) {
+        flex-direction: column;
+        gap: ${(props) => props.theme.margins.medium};
+        text-align: center;
+        
+        .actions {
+            justify-content: center;
+        }
+    }
+`
+
+export default StyledEmptyFolder
---
+++ src/components/elements/emptyFolder/EmptyFolder.tsx
@@ -0,0 +1,35 @@
+import StyledEmptyFolder from './EmptyFolder.styles'
+import Button from '../../ui/button/Button'
+
+import imageEmptyFolder from '../../../assets/images/img-empty-folder.svg'
+import iconBack from '../../../assets/icons/icon-back.svg'
+import iconUpload from '../../../assets/icons/icon-upload.svg'
+
+const EmptyFolder = () => {
+    return <StyledEmptyFolder>
+        <div className="image">
+            <img src={imageEmptyFolder} alt="" />
+        </div>
+
+        <div className="info">
+            <h2>Ten folder jest pusty!</h2>
+
+            <p>Możesz przesłać pliki na dysk poprzez przeciągnięcie i opuszczenie je na okno przeglądarki.</p>
+
+            <div className="actions">
+                <Button $variant='secondary' className='back-button'>
+                    <img src={iconBack} alt='Cofnij się' />
+                </Button>
+
+                <Button>
+                    <img src={iconUpload} alt='Prześlij pliki tutaj' />
+
+                    Prześlij pliki tutaj
+                </Button>
+            </div>
+        </div>
+
+    </StyledEmptyFolder>
+}
+
+export default EmptyFolder
