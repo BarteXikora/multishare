349a1af84f05fb0f3fbe1f4092ac7ccae7352507
BarteXikora <sbartek50@gmail.com>
2024-01-05 23:18:20 +0100

    Added the redux Provider to the test-utils.tsx.

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,12 +1,29 @@
 import { ReactElement } from 'react'
 import { render, RenderOptions } from '@testing-library/react'
+
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 import AppTheme from './theme/AppTheme'
+
+type childrenType = {
+    children?: string | JSX.Element | JSX.Element[] | undefined
+}
+
+const Providers = ({ children }: childrenType) => {
+    return (
+        <Provider store={store}>
+            <AppTheme>
+                {children}
+            </AppTheme>
+        </Provider>
+    )
+}
 
 const customRender = (
     ui: ReactElement,
     options?: Omit<RenderOptions, 'wrapper'>
 
-) => render(ui, { wrapper: AppTheme, ...options })
+) => render(ui, { wrapper: Providers, ...options })
 
 export * from '@testing-library/react'
 export { customRender as render }
