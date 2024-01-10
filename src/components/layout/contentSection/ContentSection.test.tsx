c38305ee03a2ba397274f5674374ea7b5fd0bb4a
Bartłomiej Sikora <sbartek50@gmail.com>
2024-01-10 14:29:05 +0100

    Created tests for the ContentSection component.

--- src/components/layout/contentSection/ContentSection.test.tsx
+++ src/components/layout/contentSection/ContentSection.test.tsx
@@ -1,5 +1,10 @@
-import { render, screen } from '../../../test-utils'
+import { render, screen, act } from '../../../test-utils'
+import user from '@testing-library/user-event'
+
 import ContentSection from './ContentSection'
+
+import { store } from '../../../store/store'
+import { setTreeLocation } from '../../../store/features/contentSlice/contentSlice'
 
 describe('Content Section', () => {
 
@@ -7,6 +12,40 @@ describe('Content Section', () => {
         render(<ContentSection />)
         const mainElement = screen.getByRole('main')
         expect(mainElement).toBeInTheDocument()
+    })
+
+    test('renders folders section', () => {
+        render(<ContentSection />)
+
+        const headerElement = screen.getByRole('heading', { level: 2, name: 'Foldery:' })
+        const buttonsElements = screen.getAllByRole('button')
+
+        expect(headerElement).toBeInTheDocument()
+        expect(buttonsElements).toHaveLength(4)
+    })
+
+    test('changes location after double click on folder', async () => {
+        user.setup()
+
+        render(<ContentSection />)
+
+        const privateFolderButtonElement = screen.getByRole('button', { name: /Prywatne/ })
+        await user.dblClick(privateFolderButtonElement)
+
+        const picturesFolderButtonElement = screen.getByRole('button', { name: /Obrazy/ })
+        expect(picturesFolderButtonElement).toBeInTheDocument()
+    })
+
+    test('renders files section', () => {
+        render(<ContentSection />)
+
+        act(() => {
+            store.dispatch(setTreeLocation(5))
+        })
+
+        const headerElement = screen.getByRole('heading', { level: 2, name: 'Pliki:' })
+
+        expect(headerElement).toBeInTheDocument()
     })
 
 })
