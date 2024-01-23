0031438dfb1bb52095dad929273195fb238988a5
Bartłomiej Sikora <sbartek50@gmail.com>
2024-01-23 14:10:13 +0100

    Created the setSelected reducer.

--- src/store/features/contentSlice/contentSlice.ts
+++ src/store/features/contentSlice/contentSlice.ts
@@ -3,14 +3,16 @@ import { createSlice } from '@reduxjs/toolkit'
 import initialState from './initialState'
 
 import setTreeLocationReducer from './reducers/setTreeLocation/setTreeLocation'
+import setSelectedReducer from './reducers/setSelected/setSelected'
 
 export const contentSlice = createSlice({
     name: 'contentSlice',
     initialState,
     reducers: {
-        setTreeLocation: setTreeLocationReducer
+        setTreeLocation: setTreeLocationReducer,
+        setSelected: setSelectedReducer
     }
 })
 
 export default contentSlice.reducer
-export const { setTreeLocation } = contentSlice.actions
+export const { setTreeLocation, setSelected } = contentSlice.actions
