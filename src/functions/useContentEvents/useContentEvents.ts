62bdae78227c2c7fbcc1439ec69c0fcf133a6ee5
Bartłomiej Sikora <sbartek50@gmail.com>
2024-01-23 17:21:28 +0100

    Added functionality of selecting a range of elements with shift key pressed.

--- src/functions/useContentEvents/useContentEvents.ts
+++ src/functions/useContentEvents/useContentEvents.ts
@@ -2,11 +2,51 @@ import { useEffect } from 'react'
 import { useSelector, useDispatch } from '../../store/store'
 import { setSelected } from '../../store/features/contentSlice/contentSlice'
 
-import { selectedType } from '../../store/features/contentSlice/contentSlice.types'
+import { selectedType, contentDisplayType } from '../../store/features/contentSlice/contentSlice.types'
 
 type ElementType = 'FOLDER' | 'FILE'
 
+const getRangeOfElements =
+    (
+        currentFolder: contentDisplayType,
+        first: { type: 'FOLDER' | 'FILE', id: number },
+        last: { type: 'FOLDER' | 'FILE', id: number }
+
+    ): { folders?: number[], files?: number[] } => {
+
+        const flatContent: { type: 'FOLDER' | 'FILE', id: number }[] = []
+
+        if (currentFolder) {
+            if (currentFolder.folders)
+                currentFolder.folders.forEach(folder => flatContent.push({ type: 'FOLDER', id: folder.id }))
+
+            if (currentFolder.files)
+                currentFolder.files.forEach(file => flatContent.push({ type: 'FILE', id: file.id }))
+        }
+
+        let firstIndex = flatContent.findIndex(element => element.type === first.type && element.id === first.id)
+        let lastIndex = flatContent.findIndex(element => element.type === last.type && element.id === last.id)
+
+        if (firstIndex === -1 || lastIndex === -1) return {}
+
+        if (lastIndex < firstIndex) {
+            const _last = lastIndex
+
+            lastIndex = firstIndex
+            firstIndex = _last
+        }
+
+        const elementsOfRange = flatContent.slice(firstIndex, lastIndex + 1)
+
+        const result: { folders: number[], files: number[] } = { folders: [], files: [] }
+
+        elementsOfRange.forEach(element => element.type === 'FOLDER' ? result.folders.push(element.id) : result.files.push(element.id))
+
+        return result
+    }
+
 const useContentEvents = () => {
+    const currentFolder = useSelector(state => state.content.currentFolder)
     const selected = useSelector(state => state.content.selected)
     const currentPath = useSelector(state => state.content.currentPath)
     const dispatch = useDispatch()
@@ -25,7 +65,26 @@ const useContentEvents = () => {
         let newSelected: selectedType = { selectionStart: null }
 
         if (event.shiftKey) {
-            return
+            newSelected = { ...selected }
+
+            if (selected.selectionStart === null) {
+                switch (type) {
+                    case 'FOLDER':
+                        newSelected.folders = [id]
+                        newSelected.selectionStart = { type: 'FOLDER', id }
+
+                        break
+
+                    case 'FILE':
+                        newSelected.files = [id]
+                        newSelected.selectionStart = { type: 'FILE', id }
+                }
+
+            } else {
+                const range = getRangeOfElements({ ...currentFolder }, selected.selectionStart, { type, id })
+
+                newSelected = { ...newSelected, ...range }
+            }
 
         } else if (event.ctrlKey) {
             newSelected = { ...selected }
