34e9410c0f83006c49fe9e675f6c416c1b8875a6
Bartłomiej Sikora <sbartek50@gmail.com>
2024-02-16 18:16:53 +0100

    Moved the details list section from the Details component to separate component.

--- src/components/layout/detailsSection/details/Details.tsx
+++ src/components/layout/detailsSection/details/Details.tsx
@@ -1,17 +1,13 @@
 import StyledDetails from './Details.styles'
-import Moment from 'react-moment'
 
 import PreviewSection from './previewSection/PreviewSection'
 import NameSection from './nameSection/NameSection'
 import InsideContentSection from './insideContentSection/InsideContentSection'
+import DetailsListSection from './detailsListSection/DetailsListSection'
 
 import { contentType } from '../../../../store/features/detailsSectionSlice/initialState.types'
 
 const Details = ({ content }: { content: contentType }) => {
-    if (content.type === 'EMPTY') return null
-
-    const { type, data } = content
-
     return <StyledDetails>
         <PreviewSection content={content} />
 
@@ -19,60 +15,7 @@ const Details = ({ content }: { content: contentType }) => {
 
         <InsideContentSection content={content} />
 
-
-
-
-
-
-        {
-            (type === 'FOLDER' || type === 'FILE') && <div className="section section-details">
-                <h4>Szczegóły:</h4>
-
-                <div className="details-table">
-                    <div className="detail-row">
-                        <div className="detail-name">Typ:</div>
-
-                        <div className="detail-value">__WIP...</div>
-                    </div>
-
-                    {
-                        type === 'FILE' && data.details.fileSizeBites && <div className="detail-row">
-                            <div className="detail-name">Rozmiar pliku:</div>
-
-                            <div className="detail-value">
-                                {data.details.fileSizeBites} B
-                            </div>
-                        </div>
-                    }
-
-                    {
-                        data.details.createdDate && <div className="detail-row">
-                            <div className="detail-name">Data utworzenia:</div>
-
-                            <div className="detail-value">
-                                <Moment format='D.MM.yyyy, HH:mm.ss'>
-                                    {data.details.createdDate}
-                                </Moment>
-                            </div>
-                        </div>
-                    }
-
-                    {
-                        data.details.lastModificationDate && <div className="detail-row">
-                            <div className="detail-name">Data ostatniej modyfikacji:</div>
-
-                            <div className="detail-value">
-                                <Moment format='D.MM.yyyy, HH:mm.ss'>
-                                    {data.details.lastModificationDate}
-                                </Moment>
-                            </div>
-                        </div>
-                    }
-                </div>
-            </div>
-        }
-
-
+        <DetailsListSection content={content} />
     </StyledDetails>
 }
 
---
+++ src/components/layout/detailsSection/details/detailsListSection/DetailsListSection.tsx
@@ -0,0 +1,57 @@
+import Moment from 'react-moment'
+
+import { contentType } from '../../../../../store/features/detailsSectionSlice/initialState.types'
+
+const DetailsListSection = ({ content }: { content: contentType }) => {
+    if (content.type !== 'FOLDER' && content.type !== 'FILE') return null
+
+    const { type, data } = content
+
+    return <div className="section section-details-list">
+        <h4>Szczegóły:</h4>
+
+        <div className="details-table">
+            <div className="detail-row">
+                <div className="detail-name">Typ:</div>
+
+                <div className="detail-value">__WIP...</div>
+            </div>
+
+            {
+                type === 'FILE' && data.details.fileSizeBites && <div className="detail-row">
+                    <div className="detail-name">Rozmiar pliku:</div>
+
+                    <div className="detail-value">
+                        {data.details.fileSizeBites} B
+                    </div>
+                </div>
+            }
+
+            {
+                data.details.createdDate && <div className="detail-row">
+                    <div className="detail-name">Data utworzenia:</div>
+
+                    <div className="detail-value">
+                        <Moment format='D.MM.yyyy, HH:mm.ss'>
+                            {data.details.createdDate}
+                        </Moment>
+                    </div>
+                </div>
+            }
+
+            {
+                data.details.lastModificationDate && <div className="detail-row">
+                    <div className="detail-name">Data ostatniej modyfikacji:</div>
+
+                    <div className="detail-value">
+                        <Moment format='D.MM.yyyy, HH:mm.ss'>
+                            {data.details.lastModificationDate}
+                        </Moment>
+                    </div>
+                </div>
+            }
+        </div>
+    </div>
+}
+
+export default DetailsListSection
