30a9e614901ea203be472ab420e9471f0d3fe2c0
Bartłomiej Sikora <sbartek50@gmail.com>
2024-02-17 14:31:37 +0100

    Fixed styles of the BodyColumns component.

--- src/components/layout/bodyColumns/BodyColumns.styles.tsx
+++ src/components/layout/bodyColumns/BodyColumns.styles.tsx
@@ -4,17 +4,22 @@ const StyledBodyColumns = styled.div`
     position: relative;
     display: flex;
     gap: ${(props) => props.theme.margins.big};
-    padding: ${(props) => props.theme.margins.sectionBig};
     padding-top: 98px;
     box-sizing: border-box;
 
     .main-column {
         width: 100%;
+        padding: ${(props) => props.theme.margins.sectionBig};
+        padding-top: 0;
+        padding-right: 0;
     }
 
     .aside-column {
         width: 300px;
         flex-shrink: 0;
+        padding: ${(props) => props.theme.margins.sectionBig};
+        padding-top: 0;
+        padding-left: 0;
     }
 
     @media (max-width: ${(props) => props.theme.screenBreakpoints.desktop}) {
