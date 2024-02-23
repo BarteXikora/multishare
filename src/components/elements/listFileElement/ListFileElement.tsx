1931d1fc3c7fbcd28a1fee4a3bb529619e592675
Bartłomiej Sikora <sbartek50@gmail.com>
2024-02-23 12:07:39 +0100

    Moved the files list elements to the separate component.

---
+++ src/components/elements/listFileElement/ListFileElement.tsx
@@ -0,0 +1,41 @@
+import Moment from 'react-moment'
+import getFileTypeName from '../../../functions/getFileTypeName/getFileTypeName'
+import getDataWithUnit from '../../../functions/getDataWithUnit/getDataWithUnit'
+
+import iconStar from '../../../assets/icons/icon-star-color.svg'
+
+type ListFileElementType = {
+    name: string
+    isStar: boolean
+    lastModificationDate?: Date
+    extension: string
+    fileSizeBites?: number
+}
+
+const ListFileElement = ({ name, isStar, lastModificationDate, extension, fileSizeBites }: ListFileElementType) => {
+    return <div className="list-element list-grid">
+        <div>
+            <div className="icon-placeholder"></div>
+
+            <b>{name}</b>
+        </div>
+
+        <div>
+            {isStar && <img src={iconStar} alt='Oznaczono gwiazdką' />}
+        </div>
+
+        <div>
+            <Moment format='D.MM.yyyy, HH:mm:ss'>{lastModificationDate}</Moment>
+        </div>
+
+        <div>
+            {getFileTypeName(extension)}
+        </div>
+
+        <div>
+            {fileSizeBites ? getDataWithUnit(fileSizeBites) : 'Brak danych'}
+        </div>
+    </div>
+}
+
+export default ListFileElement
--- src/components/layout/contentSection/contentListView/ContentListView.tsx
+++ src/components/layout/contentSection/contentListView/ContentListView.tsx
@@ -1,15 +1,10 @@
 import StyledContentListView from './ContentListView.styles'
-import Moment from 'react-moment'
-import getFileTypeName from '../../../../functions/getFileTypeName/getFileTypeName'
-import getDataWithUnit from '../../../../functions/getDataWithUnit/getDataWithUnit'
+import { useSelector } from '../../../../store/store'
 
 import ListHeader from '../../../elements/listHeader/ListHeader'
 import ListFolderElement from '../../../elements/listFolderElement/ListFolderElement'
-
-import { useSelector } from '../../../../store/store'
+import ListFileElement from '../../../elements/listFileElement/ListFileElement'
 import EmptyFolder from '../../../elements/emptyFolder/EmptyFolder'
-
-import iconStar from '../../../../assets/icons/icon-star-color.svg'
 
 const ContentListView = () => {
     const content = useSelector(state => state.content.currentFolder)
@@ -33,29 +28,14 @@ const ContentListView = () => {
 
                 {
                     content && content.files && content.files.length > 0 && content.files.map(file => (
-                        <div key={file.id} className="list-element list-grid">
-                            <div>
-                                <div className="icon-placeholder"></div>
-
-                                <b>{file.name}</b>
-                            </div>
-
-                            <div>
-                                {file.star && <img src={iconStar} alt='Oznaczono gwiazdką' />}
-                            </div>
-
-                            <div>
-                                <Moment format='D.MM.yyyy, HH:mm:ss'>{file.details.lastModificationDate}</Moment>
-                            </div>
-
-                            <div>
-                                {getFileTypeName(file.extension)}
-                            </div>
-
-                            <div>
-                                {file.details.fileSizeBites ? getDataWithUnit(file.details.fileSizeBites) : 'Brak danych'}
-                            </div>
-                        </div>
+                        <ListFileElement
+                            key={file.id}
+                            name={file.name}
+                            isStar={file.star}
+                            lastModificationDate={file.details.lastModificationDate}
+                            extension={file.extension}
+                            fileSizeBites={file.details.fileSizeBites}
+                        />
                     ))
                 }
             </div>
