ca67ea8bfbb4799b77a7d65ab24f24c7634278db
Bartłomiej Sikora <sbartek50@gmail.com>
2024-02-23 11:51:50 +0100

    Created the content list in the ContentListView component.

--- src/components/layout/contentSection/contentListView/ContentListView.styles.tsx
+++ src/components/layout/contentSection/contentListView/ContentListView.styles.tsx
@@ -6,26 +6,74 @@ const StyledContentListView = styled.div`
     pointer-events: none;
 
     .list-grid {
+        padding: ${(props) => props.theme.margins.sectionSmall};
+        align-items: center;
         display: grid;
         grid-template-columns: 4fr 1fr 2fr 3fr 2fr;
+
+        div {
+            display: flex;
+            align-items: center;
+        }
     }
 
     .list-header {
-        padding: ${(props) => props.theme.margins.sectionSmall};
-        align-items: center;
         background-color: ${(props) => props.theme.colors.gray6};
         color: ${(props) => props.theme.colors.white};
         font-size: ${(props) => props.theme.fontSizes.default};
         font-weight: bold;
         border-radius: ${(props) => props.theme.borderRadiuses.big + ' ' + props.theme.borderRadiuses.big} 0 0;
 
-        div {
-            display: flex;
-            align-items: center;
-        }
-
         .icon-star {
             width: 20px;
+        }
+    }
+
+    .list-element {
+        background-color: ${(props) => props.theme.colors.gray2};
+        transition: ${(props) => props.theme.transition};
+        pointer-events: all;
+        cursor: pointer;
+
+        div {
+            gap: ${(props) => props.theme.margins.medium};
+
+            &:not(:first-of-type) {
+                margin-left: ${(props) => props.theme.margins.medium};
+            }
+        }
+
+        .icon-placeholder {
+            width: 26px;
+            height: 26px;
+        }
+
+        &.selected {
+            background-color: ${(props) => props.theme.colors.primary2};
+
+            &:nth-of-type(odd) {
+                background-color: ${(props) => props.theme.colors.primary3};
+            }
+
+            &:hover {
+                background-color: ${(props) => props.theme.colors.primary2} !important;
+
+                &:nth-of-type(odd) {
+                    background-color: ${(props) => props.theme.colors.primary3} !important;
+                }
+            }
+        }
+
+        &:hover {
+            background-color: ${(props) => props.theme.colors.gray4} !important;
+        }
+
+        &:nth-of-type(odd) {
+            background-color: ${(props) => props.theme.colors.gray3};
+        }
+
+        &:last-of-type {
+            border-radius: 0 0 ${(props) => props.theme.borderRadiuses.big + ' ' + props.theme.borderRadiuses.big};
         }
     }
 `
--- src/components/layout/contentSection/contentListView/ContentListView.tsx
+++ src/components/layout/contentSection/contentListView/ContentListView.tsx
@@ -1,9 +1,15 @@
 import StyledContentListView from './ContentListView.styles'
+import Moment from 'react-moment'
+import getFileTypeName from '../../../../functions/getFileTypeName/getFileTypeName'
+import getDataWithUnit from '../../../../functions/getDataWithUnit/getDataWithUnit'
 
 import ListHeader from '../../../elements/listHeader/ListHeader'
 
 import { useSelector } from '../../../../store/store'
 import EmptyFolder from '../../../elements/emptyFolder/EmptyFolder'
+
+import iconFolder from '../../../../assets/icons/icon-project.svg'
+import iconStar from '../../../../assets/icons/icon-star-color.svg'
 
 const ContentListView = () => {
     const content = useSelector(state => state.content.currentFolder)
@@ -13,7 +19,57 @@ const ContentListView = () => {
             (content && (content.folders || content.files)) && <div className="content-list">
                 <ListHeader />
 
-                <div className="list-body list-grid"></div>
+                {
+                    content && content.folders && content.folders.length > 0 && content.folders.map(folder => (
+                        <div key={folder.id} className="list-element list-grid">
+                            <div>
+                                <img src={iconFolder} alt='Folder:' />
+
+                                <b>{folder.name}</b>
+                            </div>
+
+                            <div>
+                                {folder.star && <img src={iconStar} alt='Oznaczono gwiazdką' />}
+                            </div>
+
+                            <div>
+                                <Moment format='D.MM.yyyy, HH:mm:ss'>{folder.details.lastModificationDate}</Moment>
+                            </div>
+
+                            <div>
+                                {folder.insideContent.folders + folder.insideContent.files > 0 ? 'Folder plików' : 'Pusty folder'}
+                            </div>
+                        </div>
+                    ))
+                }
+
+                {
+                    content && content.files && content.files.length > 0 && content.files.map(file => (
+                        <div key={file.id} className="list-element list-grid">
+                            <div>
+                                <div className="icon-placeholder"></div>
+
+                                <b>{file.name}</b>
+                            </div>
+
+                            <div>
+                                {file.star && <img src={iconStar} alt='Oznaczono gwiazdką' />}
+                            </div>
+
+                            <div>
+                                <Moment format='D.MM.yyyy, HH:mm:ss'>{file.details.lastModificationDate}</Moment>
+                            </div>
+
+                            <div>
+                                {getFileTypeName(file.extension)}
+                            </div>
+
+                            <div>
+                                {file.details.fileSizeBites ? getDataWithUnit(file.details.fileSizeBites) : 'Brak danych'}
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         }
 
