9d750214aa30da7aee0bcf43844451a50558925f
Bartłomiej Sikora <sbartek50@gmail.com>
2024-02-24 13:14:36 +0100

    Created DetailsSection components content except of main content section.

--- src/components/layout/detailsSection/DetailsSection.tsx
+++ src/components/layout/detailsSection/DetailsSection.tsx
@@ -1,5 +1,30 @@
+import { useSelector, useDispatch } from '../../../store/store'
+import { toggle } from '../../../store/features/detailsSectionSlice/detailsSectionSlice'
+
+import StyledDetailsSection from './DetailsSection.styles'
+import Button from '../../ui/button/Button'
+import ProcessPill from '../../elements/processPill/ProcessPill'
+
+import iconClose from '../../../assets/icons/icon-close.svg'
+
 const DetailsSection = () => {
-    return <>details</>
+    const isShown = useSelector(state => state.detailsSection.isShown)
+    const dispatch = useDispatch()
+
+    return <StyledDetailsSection className={`${isShown ? 'shown' : ''}`}>
+
+        <Button className='close-button' $variant='wrong' $size='big' onClick={() => dispatch(toggle(false))}>
+            <img src={iconClose} alt='Zamknij szczegóły' />
+        </Button>
+
+        <section className='main-content'>
+            main content
+        </section>
+
+        <div className="process-pill">
+            <ProcessPill />
+        </div>
+    </StyledDetailsSection>
 }
 
 export default DetailsSection
