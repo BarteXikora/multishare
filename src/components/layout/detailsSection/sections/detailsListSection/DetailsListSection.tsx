4b29747ec08db2d63e32575c28aea6beca77f5c7
Bartłomiej Sikora <sbartek50@gmail.com>
2024-02-24 15:57:57 +0100

    Created the new DetailsListSection component.

---
+++ src/components/layout/detailsSection/sections/detailsListSection/DetailsListSection.tsx
@@ -0,0 +1,59 @@
+import StyledDetailsListSection from './DetailsListSection.styles'
+
+import Moment from 'react-moment'
+import getDataWithUnit from '../../../../../functions/getDataWithUnit/getDataWithUnit'
+
+type DetailsListSectionType = {
+    type: string
+    fileSizeBites?: number
+    createdDate?: Date
+    lastModificationDate?: Date
+}
+
+const DetailsListSection = ({ type, fileSizeBites, createdDate, lastModificationDate }: DetailsListSectionType) => {
+    return <StyledDetailsListSection>
+        <h4>Szczegóły:</h4>
+
+        <div className="details-table">
+            <div className="detail-row">
+                <div className="detail-name">Typ:</div>
+
+                <div className="detail-value">{type}</div>
+            </div>
+
+            {
+                fileSizeBites && <div className="detail-row">
+                    <div className="detail-name">Rozmiar pliku:</div>
+
+                    <div className="detail-value">{getDataWithUnit(fileSizeBites)}</div>
+                </div>
+            }
+
+            {
+                createdDate && <div className="detail-row">
+                    <div className="detail-name">Data utworzenia:</div>
+
+                    <div className="detail-value">
+                        <Moment format='D.MM.yyyy, HH:mm:ss'>
+                            {createdDate}
+                        </Moment>
+                    </div>
+                </div>
+            }
+
+            {
+                lastModificationDate && <div className="detail-row">
+                    <div className="detail-name">Data ostatniej modyfikacji:</div>
+
+                    <div className="detail-value">
+                        <Moment format='D.MM.yyyy, HH:mm:ss'>
+                            {lastModificationDate}
+                        </Moment>
+                    </div>
+                </div>
+            }
+        </div>
+    </StyledDetailsListSection>
+}
+
+export default DetailsListSection
