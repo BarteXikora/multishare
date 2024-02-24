ecee8c952819e0ff574369044dfd3cfd42a1cfe1
Bartłomiej Sikora <sbartek50@gmail.com>
2024-02-24 13:05:33 +0100

    Moved the DetailsSection componnt to the temporary dev folder and prepared new SetailsSection component.

---
+++ src/components/layout/__detailsSection/DetailsSection.tsx
@@ -0,0 +1,46 @@
+import { useSelector, useDispatch } from '../../../store/store'
+import { toggle } from '../../../store/features/detailsSectionSlice/detailsSectionSlice'
+import useInvokeDetails from '../../../functions/useInvokeDetails/useInvokeDetails'
+
+import StyledDetailsSection from './DetailsSection.styles'
+import Button from '../../ui/button/Button'
+
+import DropSection from './dropSection/DropSection'
+import Details from './details/Details'
+
+// import SingleFolderDetails from './singleFolderDetails/SingleFolderDetails'
+// import SingleFileDetails from './singleFileDetails/SingleFileDetails'
+
+import ProcessPill from '../../elements/processPill/ProcessPill'
+
+import iconClose from '../../../assets/icons/icon-close.svg'
+
+const DetailsSection = () => {
+    const isShown = useSelector(state => state.detailsSection.isShown)
+    const content = useSelector(state => state.detailsSection.content)
+    const dispatch = useDispatch()
+
+    useInvokeDetails()
+
+    return <StyledDetailsSection className={`${isShown ? 'shown' : ''}`}>
+
+        <Button className='close-button' $variant='wrong' $size='big' onClick={() => dispatch(toggle(false))}>
+            <img src={iconClose} alt='Zamknij szczegóły' />
+        </Button>
+
+        <section className="main-content">
+            {
+                content.type === 'EMPTY' ?
+                    <DropSection />
+                    :
+                    <Details content={content} />
+            }
+        </section>
+
+        <div className="process-pill">
+            <ProcessPill />
+        </div>
+    </StyledDetailsSection>
+}
+
+export default DetailsSection
