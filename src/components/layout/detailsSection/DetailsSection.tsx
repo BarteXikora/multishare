580445cbb076854f11ee345c3d996b7d6171d1f7
Bartłomiej Sikora <sbartek50@gmail.com>
2024-02-25 13:27:47 +0100

    Placed all the details types components in the DetailsSection component.

--- src/components/layout/detailsSection/DetailsSection.tsx
+++ src/components/layout/detailsSection/DetailsSection.tsx
@@ -1,15 +1,26 @@
 import { useSelector, useDispatch } from '../../../store/store'
 import { toggle } from '../../../store/features/detailsSectionSlice/detailsSectionSlice'
+import useInvokeDetails from '../../../functions/useInvokeDetails/useInvokeDetails'
 
 import StyledDetailsSection from './DetailsSection.styles'
+import NothingSelectedDetails from './detailsTypes/nothingSelectedDetails/NothingSelectedDetails'
+import SingleFolderDetails from './detailsTypes/singleFolderDetails/SingleFolderDetails'
+import SingleFileDetails from './detailsTypes/singleFileDetails/SingleFileDetails'
+import MultipleDetails from './detailsTypes/multipleDetails/MultipleDetails'
 import Button from '../../ui/button/Button'
 import ProcessPill from '../../elements/processPill/ProcessPill'
 
 import iconClose from '../../../assets/icons/icon-close.svg'
 
 const DetailsSection = () => {
+    const project = useSelector(state => state.content.project)
+    const isHomeFolder = useSelector(state => state.content.currentPath.length === 0)
     const isShown = useSelector(state => state.detailsSection.isShown)
+    const content = useSelector(state => state.detailsSection.content)
+
     const dispatch = useDispatch()
+
+    useInvokeDetails()
 
     return <StyledDetailsSection className={`${isShown ? 'shown' : ''}`}>
 
@@ -18,7 +29,10 @@ const DetailsSection = () => {
         </Button>
 
         <section className='main-content'>
-            main content
+            {content.type === 'EMPTY' && <NothingSelectedDetails isHomeFolder={isHomeFolder} project={project} />}
+            {content.type === 'FOLDER' && <SingleFolderDetails content={content} />}
+            {content.type === 'FILE' && <SingleFileDetails content={content} />}
+            {content.type === 'MULTIPLE' && <MultipleDetails content={content} />}
         </section>
 
         <div className="process-pill">
