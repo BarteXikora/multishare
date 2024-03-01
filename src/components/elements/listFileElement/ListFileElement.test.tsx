d14456d41a2e056f48016827174e6ef6051d5fb0
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-01 17:01:06 +0100

    Cteated test for the ListFileElement component.

---
+++ src/components/elements/listFileElement/ListFileElement.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '../../../test-utils'
+import ListFileElement from './ListFileElement'
+
+const _listFileElementProps = {
+    name: 'Test',
+    isStar: false,
+    lastModificationDate: '0',
+    extension: 'TEST',
+    fileSizeBites: 1024,
+    isSelected: false,
+    isFolderIconOffset: false,
+    onClick: () => null,
+    onTouchStart: () => null,
+    onTouchEnd: () => null
+}
+
+describe('List File Element', () => {
+
+    test('renders correctly', () => {
+        render(<ListFileElement {..._listFileElementProps} />)
+
+        const buttonElement = screen.getByRole('button')
+
+        expect(buttonElement).toBeInTheDocument()
+    })
+
+})
