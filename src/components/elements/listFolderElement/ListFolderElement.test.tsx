4e597f19a67e267b4a32b7b62419ed41983fd353
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-01 16:42:15 +0100

    Created the ListFolderElement components test.

---
+++ src/components/elements/listFolderElement/ListFolderElement.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '../../../test-utils'
+import ListFolderElement from './ListFolderElement'
+
+const _ListFolderElementTypeProps = {
+    name: 'Test',
+    isStar: false,
+    lastModificationDate: '0',
+    isEmpty: false,
+    isSelected: false,
+    onClick: () => null,
+    onDoubleClick: () => null,
+    onTouchStart: () => null,
+    onTouchEnd: () => null
+}
+
+describe('List Folder Element', () => {
+
+    test('renders correctly', () => {
+        render(<ListFolderElement {..._ListFolderElementTypeProps} />)
+
+        const buttonElement = screen.getByRole('button')
+
+        expect(buttonElement).toBeInTheDocument()
+    })
+
+    test('renders date in proper format', async () => {
+        render(<ListFolderElement {..._ListFolderElementTypeProps} lastModificationDate='1-1-2024 12:00:00' />)
+
+        const dateElement = await screen.findByText('01.01.2024, 12:00:00')
+
+        expect(dateElement).toBeInTheDocument()
+    })
+
+})
