7f21dff8b29e8ab728a7652bf9a2e8de867ee4a2
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-05 17:58:11 +0100

    Created the initialize reducer in the contentSlice.

--- src/store/features/contentSlice/contentSlice.ts
+++ src/store/features/contentSlice/contentSlice.ts
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 import initialState from './initialState'
 
+import initializeReducer from './reducers/initialize/initialize'
 import setTreeLocationReducer from './reducers/setTreeLocation/setTreeLocation'
 import setSelectedReducer from './reducers/setSelected/setSelected'
 
@@ -9,10 +10,11 @@ export const contentSlice = createSlice({
     name: 'contentSlice',
     initialState,
     reducers: {
+        initialize: initializeReducer,
         setTreeLocation: setTreeLocationReducer,
         setSelected: setSelectedReducer
     }
 })
 
 export default contentSlice.reducer
-export const { setTreeLocation, setSelected } = contentSlice.actions
+export const { initialize, setTreeLocation, setSelected } = contentSlice.actions
---
+++ src/store/features/contentSlice/reducers/initialize/initialize.ts
@@ -0,0 +1,6 @@
+import { PayloadAction } from '@reduxjs/toolkit'
+import { initialStateType } from '../../contentSlice.types'
+
+const initialize = (state: initialStateType, action: PayloadAction) => { }
+
+export default initialize
