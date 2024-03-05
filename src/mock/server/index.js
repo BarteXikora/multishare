e054d7bfe1b70c5a34a115c721de59220694370a
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-05 17:56:00 +0100

    Created the get_content event in the mocked server.

--- src/mock/server/index.js
+++ src/mock/server/index.js
@@ -11,8 +11,14 @@ const { Server } = require('socket.io')
 
 const io = new Server(server, { cors: { origin: 'http://localhost:3000' } })
 
+const exampleContent = require('./data/exampleContent')
+let theContent = exampleContent
+
 io.on('connection', (socket) => {
     console.log('user logged in, id:', socket.id)
+
+    socket.on('get_content', () => socket.emit('content', theContent))
+
 })
 
 server.listen(3001, () => console.log('server is listening on port 3001'))
