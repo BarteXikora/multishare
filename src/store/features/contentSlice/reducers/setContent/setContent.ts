8eb4ad4e99aad9b87758395b1fd7ef1cc3758013
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-05 22:27:57 +0100

    Added changing status value in the setContent reducer.

--- src/store/features/contentSlice/reducers/setContent/setContent.ts
+++ src/store/features/contentSlice/reducers/setContent/setContent.ts
@@ -3,6 +3,7 @@ import { contentType, initialStateType } from '../../contentSlice.types'
 
 const setContent = (state: initialStateType, action: PayloadAction<contentType>) => {
     state.loadedContent = action.payload
+    state.status = 'LOADED'
 }
 
 export default setContent
