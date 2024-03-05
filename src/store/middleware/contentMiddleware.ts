8ccafbf195bdc4a84b7f9defcdf5981a9843a9cc
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-05 18:00:47 +0100

    Created the middleware for the initialize reducer to get content from the mocked server.

--- src/store/middleware/contentMiddleware.ts
+++ src/store/middleware/contentMiddleware.ts
@@ -1,12 +1,28 @@
 import { Dispatch } from '@reduxjs/toolkit'
+import socket from '../../api/socket'
+import { contentType } from '../features/contentSlice/contentSlice.types'
+import { setContent } from '../features/contentSlice/contentSlice'
 
-type propsType = {
+type paramsType = {
     dispatch: Dispatch
 }
 
 const contentMiddleware = () => {
-    return (props: propsType) => (next: any) => (action: any) => {
-        console.log('middleware')
+    return (params: paramsType) => (next: any) => (action: any) => {
+        switch (action.type) {
+            case 'contentSlice/initialize': {
+                socket.on('content', (data: any) => {
+                    const recivedContent: contentType = {
+                        folders: (data && data.folders) ? data.folders : [],
+                        files: (data && data.files) ? data.files : []
+                    }
+
+                    params.dispatch(setContent(recivedContent))
+                })
+
+                socket.emit('get_content')
+            }
+        }
 
         next(action)
     }
