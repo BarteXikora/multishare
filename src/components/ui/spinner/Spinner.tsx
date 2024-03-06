65a500996d84930916ccca643c518c9f41d990a7
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-06 17:33:28 +0100

    Created the Spinner component.

---
+++ src/components/ui/spinner/Spinner.tsx
@@ -0,0 +1,12 @@
+import StyledSpinner from './Spinner.styles'
+
+const Spinner = () => {
+    return <StyledSpinner>
+        <div className="spinner-element"></div>
+        <div className="spinner-element"></div>
+        <div className="spinner-element"></div>
+        <div className="spinner-element"></div>
+    </StyledSpinner>
+}
+
+export default Spinner
