60e1ae594a9fc2450620da456a3924c945549656
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-06 09:22:10 +0100

    Fixed the initial state of the contentSlice.

--- src/store/features/contentSlice/initialState.ts
+++ src/store/features/contentSlice/initialState.ts
@@ -3,11 +3,10 @@ import { initialStateType } from './contentSlice.types'
 const emptyContent = { folders: [], files: [] }
 
 const initialState: initialStateType = {
-    status: 'LOADING',
     project: { id: 0, name: 'Moje pliki' },
     currentPath: [],
     currentFolder: emptyContent,
-    loadedContent: emptyContent,
+    loadedContent: { status: 'LOADING' },
     selected: { ...emptyContent, selectionStart: null }
 }
 
