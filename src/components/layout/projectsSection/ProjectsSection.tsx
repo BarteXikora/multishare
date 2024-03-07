3e1db08fb4d4375dbc9d4f7c2c9ddcc0fabace44
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-07 19:31:33 +0100

    Used allProjects value in the ProjectsSection component.

--- src/components/layout/projectsSection/ProjectsSection.tsx
+++ src/components/layout/projectsSection/ProjectsSection.tsx
@@ -1,9 +1,21 @@
 import StyledProjectsSection from './ProjectsSection.tyles'
 import Project from '../../elements/project/Project'
 
+import { useSelector } from '../../../store/store'
+
 const ProjectsSection = () => {
+    const projects = useSelector(state => state.project.allProjects)
+
     return <StyledProjectsSection>
-        <Project name='Moje pliki' description='Moje pliki osobiste' />
+        {projects.status === 'LOADING' && <>wczytywanie...</>}
+
+        {projects.status === 'ERROR' && <>wystąpił błąd</>}
+
+        {
+            projects.status === 'READY' && projects.content.map(project => (
+                <Project key={project.id} name={project.name} />
+            ))
+        }
     </StyledProjectsSection>
 }
 
