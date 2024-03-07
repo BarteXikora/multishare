fb3b7906cd24a3eea61ab3c67692c4f2e86d2d15
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-07 23:16:57 +0100

    Added icon value to the projects in the mocked server.

--- src/mock/server/data/projects.js
+++ src/mock/server/data/projects.js
@@ -2,14 +2,19 @@ const projects = [
     {
         id: 0,
         name: 'Moje pliki',
-        description: 'Moje pliki osobiste.'
+        description: 'Moje pliki osobiste.',
+        icon: {
+            type: 'ICON',
+            icon: 'PERSONAL_PROJECT'
+        }
     }, {
         id: 1,
         name: 'Inny projekt',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae facilis ad neque!'
-    }, {
-        id: 3,
-        name: '__TEST'
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae facilis ad neque!',
+        icon: {
+            type: 'LINK',
+            link: 'https://picsum.photos/seed/multishare/200/300'
+        }
     }
 ]
 
