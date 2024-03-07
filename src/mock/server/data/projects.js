aea38c7e47845ef0df4f305f73244fb7137396d1
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-07 22:39:18 +0100

    Added example descriptions to the projects in the mocked server.

--- src/mock/server/data/projects.js
+++ src/mock/server/data/projects.js
@@ -1,10 +1,15 @@
 const projects = [
     {
         id: 0,
-        name: 'Moje pliki'
+        name: 'Moje pliki',
+        description: 'Moje pliki osobiste.'
     }, {
         id: 1,
-        name: 'Inny projekt'
+        name: 'Inny projekt',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae facilis ad neque!'
+    }, {
+        id: 3,
+        name: '__TEST'
     }
 ]
 
