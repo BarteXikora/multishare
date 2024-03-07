cf9ff311942d265e9749b9404fdab1727b90cea3
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-07 21:37:59 +0100

    Added functionality of geting content on project select.

--- src/store/middleware/socketMiddleware.ts
+++ src/store/middleware/socketMiddleware.ts
@@ -1,6 +1,7 @@
 import { Dispatch } from '@reduxjs/toolkit'
 import socket from '../../api/socket'
 import { setProjects, setProjectsError } from '../features/projectSlice/projectSlice'
+import { setContent, setError } from '../features/contentSlice/contentSlice'
 
 type paramsType = {
     dispatch: Dispatch
@@ -18,6 +19,17 @@ const socketMiddleware = () => {
                     if (!Array.isArray(data)) dispatch(setProjectsError('Nie udało się wczytać listy projektów.'))
                     else dispatch(setProjects(data))
                 })
+
+                socket.on('content', (data: any) => {
+                    if (data === null || data === undefined) dispatch(setError('Nie udało się wczytać projektu.'))
+                    else dispatch(setContent(data))
+                })
+
+                break
+            }
+
+            case 'projectSlice/selectProject': {
+                socket.emit('enter_project', action.payload.id)
             }
         }
 
