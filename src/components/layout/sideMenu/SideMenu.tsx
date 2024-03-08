52ffa9afce69516ce70ce10564b0b8467632b8d5
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-08 08:28:00 +0100

    Moved sideMenus buttons to a separate component.

--- src/components/layout/sideMenu/SideMenu.tsx
+++ src/components/layout/sideMenu/SideMenu.tsx
@@ -2,15 +2,12 @@ import { useSelector, useDispatch } from '../../../store/store'
 import { toggle } from '../../../store/features/sideMenuSlice/sideMenuSlice'
 
 import StyledSideMenu from './SideMenu.styles'
+import ContentButtons from './contentButtons/ContentButtons'
 import Button from '../../ui/button/Button'
 import UsedSpace from '../../elements/usedSpace/UsedSpace'
 
 import iconClose from '../../../assets/icons/icon-close.svg'
 import logo from '../../../assets/images/img-logo.svg'
-import iconFolder from '../../../assets/icons/icon-folder.svg'
-import iconFiles from '../../../assets/icons/icon-files.svg'
-import iconStar from '../../../assets/icons/icon-star.svg'
-import iconTrash from '../../../assets/icons/icon-trash.svg'
 import iconAccount from '../../../assets/icons/icon-account.svg'
 
 const SideMenu = () => {
@@ -30,29 +27,7 @@ const SideMenu = () => {
         </section>
 
         <section className='nav-menu'>
-            <Button $variant='tertiary' $active>
-                <img src={iconFolder} alt='Dysk' />
-
-                Dysk
-            </Button>
-
-            <Button $variant='tertiary'>
-                <img src={iconFiles} alt='Wszystkie pliki' />
-
-                Wszystkie pliki
-            </Button>
-
-            <Button $variant='tertiary'>
-                <img src={iconStar} alt='Oznaczone gwiazdką' />
-
-                Oznaczone gwiazdką
-            </Button>
-
-            <Button $variant='tertiary'>
-                <img src={iconTrash} alt='Kosz' />
-
-                Kosz
-            </Button>
+            <ContentButtons />
         </section>
 
         <section className='used-space'>
