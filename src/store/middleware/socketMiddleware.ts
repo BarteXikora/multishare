b071b279079650aa4c46a3fcd8749ebc9eea90e7
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-12 10:16:24 +0100

    Created the loading content functionality in the socketMiddleware.

--- src/store/middleware/socketMiddleware.ts
+++ src/store/middleware/socketMiddleware.ts
@@ -1,5 +1,8 @@
 import { Dispatch } from '@reduxjs/toolkit'
 import { rootStateType } from '../rootReducer.types'
+import socket from '../../api/socket'
+import { setContent, setError, setTreeLocation } from '../features/contentSlice/contentSlice'
+import { selectProject } from '../features/projectSlice/projectSlice'
 
 type paramsType = {
     dispatch: Dispatch
@@ -8,6 +11,30 @@ type paramsType = {
 
 const socketMiddleware = () => {
     return (params: paramsType) => (next: any) => (action: any) => {
+        const { dispatch } = params
+
+        switch (action.type) {
+            case 'contentSlice/initializeContent': {
+                let [projectId, folderId] = action.payload.substring(9, action.payload.length).split('/')
+
+                if (projectId === '' || projectId === undefined) projectId = 0
+
+                socket.emit('enter_project', Number(projectId))
+
+                const loadContent = (data: any) => {
+                    if (data === null) return dispatch(setError('Nie udało się wczytać projektu!'))
+                    if (!data.project || !data.content) return dispatch(setError('Nie udało się wczytać projektu!'))
+
+                    dispatch(selectProject(data.project))
+                    dispatch(setContent(data.content))
+
+                    if (folderId) dispatch(setTreeLocation(Number(folderId)))
+                }
+
+                socket.once('content', loadContent)
+            }
+        }
+
         next(action)
     }
 }
