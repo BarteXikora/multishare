69e322756f4a6259fbd9d2308af06d48acaccca0
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-12 15:07:58 +0100

    Added loading and error elements to the ProjectsPage.

--- src/pages/projectsPage/ProjectsPage.tsx
+++ src/pages/projectsPage/ProjectsPage.tsx
@@ -1,17 +1,24 @@
 import { useEffect } from 'react'
-import { useDispatch } from '../../store/store'
+import { useSelector, useDispatch } from '../../store/store'
 import { initializeProjects } from '../../store/features/projectSlice/projectSlice'
 
+import LoadingPage from '../loadingPage/LoadingPage'
+import ErrorPage from '../errorPage/ErrorPage'
 import ProjectsTopBar from '../../components/layout/projectsTopBar/ProjectsTopBar'
 import ProjectsSection from '../../components/layout/projectsSection/ProjectsSection'
 
 const ProjectsPage = () => {
     const dispatch = useDispatch()
 
+    const projectsStatus = useSelector(state => state.project.allProjects)
+
     useEffect(() => {
         dispatch(initializeProjects())
 
     }, [dispatch])
+
+    if (projectsStatus.status === 'LOADING') return <LoadingPage />
+    if (projectsStatus.status === 'ERROR') return <ErrorPage error={projectsStatus.error} />
 
     return <>
         <ProjectsTopBar />
