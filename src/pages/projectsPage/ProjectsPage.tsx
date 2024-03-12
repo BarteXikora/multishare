f7c0b67989dd3d417159007d09d928709574efbf
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-12 15:07:05 +0100

    Added dispatching the initializeProjects to the ProjectsPage.

--- src/pages/projectsPage/ProjectsPage.tsx
+++ src/pages/projectsPage/ProjectsPage.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from 'react'
+import { useDispatch } from '../../store/store'
+import { initializeProjects } from '../../store/features/projectSlice/projectSlice'
+
 import ProjectsTopBar from '../../components/layout/projectsTopBar/ProjectsTopBar'
 import ProjectsSection from '../../components/layout/projectsSection/ProjectsSection'
 
 const ProjectsPage = () => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(initializeProjects())
+
+    }, [dispatch])
+
     return <>
         <ProjectsTopBar />
 
