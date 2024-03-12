f3a8f4736714aeb8652a40958c657b40bd2ea196
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-12 14:15:36 +0100

    Used the useUpdatePathName hook in the ContentSection component.

--- src/components/layout/contentSection/ContentSection.tsx
+++ src/components/layout/contentSection/ContentSection.tsx
@@ -1,24 +1,23 @@
 import { useSelector } from '../../../store/store'
 import useUpdateContent from '../../../functions/useUpdateContent/useUpdateContent'
+import useUpdatePathName from '../../../functions/useUpdatePathName/useUpdatePathName'
 
 import StyledContentSection from './ContentSection.styles'
 import ContentIconsView from './contentIconsView/ContentIconsView'
 import ContentListView from './contentListView/ContentListView'
 
 const ContentSection = () => {
-    const loaded = useSelector(state => state.content.loadedContent)
     const contentViewStyle = useSelector(state => state.view.contentViewStyle)
 
+    useUpdatePathName()
     useUpdateContent()
 
     return <StyledContentSection>
         {
-            loaded.status === 'READY' && (
-                contentViewStyle === 'ICONS' ?
-                    <ContentIconsView />
-                    :
-                    <ContentListView />
-            )
+            contentViewStyle === 'ICONS' ?
+                <ContentIconsView />
+                :
+                <ContentListView />
         }
     </StyledContentSection>
 }
