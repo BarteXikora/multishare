372330a291bb96f5f2d90fe212f80fde0072ebc6
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-13 10:19:28 +0100

    Created the setPreviewError reducer.

--- src/store/features/previewSlice/previewSlice.ts
+++ src/store/features/previewSlice/previewSlice.ts
@@ -3,15 +3,17 @@ import initialState from './initialState'
 
 import initializePreviewReducer from './reducers/initializePreview/initializePreview'
 import setPreviewReducer from './reducers/setPreview/setPreview'
+import setPreviewErrorReducer from './reducers/setPreviewError/setPreviewError'
 
 export const previewSlice = createSlice({
     name: 'previewSlice',
     initialState,
     reducers: {
         initializePreview: initializePreviewReducer,
-        setPreview: setPreviewReducer
+        setPreview: setPreviewReducer,
+        setPreviewError: setPreviewErrorReducer
     }
 })
 
 export default previewSlice.reducer
-export const { initializePreview, setPreview } = previewSlice.actions 
+export const { initializePreview, setPreview, setPreviewError } = previewSlice.actions 
