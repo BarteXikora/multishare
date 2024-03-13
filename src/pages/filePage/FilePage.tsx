1d92176fdfcbe415d23cd4a4ebf3949e24547672
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-13 10:00:15 +0100

    Added dispatching initializePreview to the FilePage.

--- src/pages/filePage/FilePage.tsx
+++ src/pages/filePage/FilePage.tsx
@@ -1,11 +1,25 @@
-import { useSelector } from '../../store/store'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from '../../store/store'
+import { useLocation } from 'react-router-dom'
+import { initializePreview } from '../../store/features/previewSlice/previewSlice'
 
 import StyledFilePage from './FilePage.styles'
 import LoadingPage from '../loadingPage/LoadingPage'
 import ErrorPage from '../errorPage/ErrorPage'
 
 const FilePage = () => {
+    const dispatch = useDispatch()
+    const location = useLocation()
+
     const preview = useSelector(state => state.preview.content)
+
+    useEffect(() => {
+        const fileId = Number(location.pathname.substring(6, location.pathname.length))
+
+        dispatch(initializePreview(fileId))
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [dispatch])
 
     if (preview.status === 'LOADING') return <LoadingPage />
     if (preview.status === 'ERROR') return <ErrorPage error={preview.error} />
