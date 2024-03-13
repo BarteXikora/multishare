7f8419d272d9e1ebbdf6078eefefa789535aea77
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-13 10:41:24 +0100

    Prepared mocked server to handle file loading.

---
+++ src/mock/server/data/files.js
@@ -0,0 +1,22 @@
+const files = [{
+    id: 6535674655366,
+    type: 'IMAGE',
+    data: 'https://picsum.photos/seed/wycieczkaa/2048/1024'
+}, {
+    id: 4567534564654,
+    type: 'IMAGE',
+    data: 'https://picsum.photos/seed/wycieczkab/2048/1024'
+}, {
+    id: 3456456765435,
+    type: 'IMAGE',
+    data: 'https://picsum.photos/seed/wycieczkac/2048/1024'
+}, {
+    id: 34563675662456,
+    type: 'TEXT',
+    data: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo consectetur illum vero non, modi 
+    repudiandae possimus earum, pariatur sint labore voluptatem natus, hic saepe debitis porro sapiente fugiat. 
+    Ipsum deleniti modi architecto vero pariatur distinctio earum tempore quidem, dolorum obcaecati ut culpa 
+    dicta voluptate fuga.`
+}]
+
+module.exports = files
