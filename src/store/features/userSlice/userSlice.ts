251d50c69b8499c2d45648557bbbd24e6ab3237d
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-19 16:00:58 +0100

    Prepared the initializeUser reducer.

--- src/store/features/userSlice/userSlice.ts
+++ src/store/features/userSlice/userSlice.ts
@@ -1,12 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit'
-
 import initialState from './initialState'
+
+import initializeUserReducer from './reducers/initializeUser/initializeUser'
 
 export const userSlice = createSlice({
     name: 'userSlice',
     initialState,
-    reducers: {}
+    reducers: {
+        initializeUser: initializeUserReducer
+    }
 })
 
 export default userSlice.reducer
-export const { } = userSlice.actions
+export const { initializeUser } = userSlice.actions
