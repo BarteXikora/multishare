933894d351116d32902bb62a5e332722fdab4d8d
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-22 14:48:41 +0100

    Created the displayType value in the contentSlice.

--- src/store/features/contentSlice/initialState.ts
+++ src/store/features/contentSlice/initialState.ts
@@ -4,6 +4,7 @@ const emptyContent = { folders: [], files: [] }
 
 const initialState: contentStateType = {
     currentPath: [],
+    displayType: 'TREE',
     currentFolder: emptyContent,
     loadedContent: { status: 'LOADING' },
     selected: { ...emptyContent, selectionStart: null }
