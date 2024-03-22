8c54a6df204e2d1ab6ff64661b72a0f1a71e86b7
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-22 15:35:10 +0100

    Added handling /files pathname to the useUpdatePathName hook

--- src/functions/useUpdatePathName/useUpdatePathName.ts
+++ src/functions/useUpdatePathName/useUpdatePathName.ts
@@ -8,20 +8,22 @@ const useUpdatePathName = () => {
     const currentFolder = useSelector(state => state.content.currentFolder)
     const project = useSelector(state => state.project.selectedProject)
     const path = useSelector(state => state.content.currentPath)
+    const displayType = useSelector(state => state.content.displayType)
 
     useEffect(() => {
-        let newPathName = '/project/'
+        let newPathName = displayType === 'TREE' ? '/project/' : '/files/'
 
         if (project) newPathName += project.id
 
-        if (path.length > 0) {
-            newPathName += '/'
+        if (displayType === 'TREE') {
+            if (path.length > 0) {
+                newPathName += '/'
 
-            if (Number.isNaN(path[path.length - 1].id)) newPathName += 'not-found'
-            else newPathName += path[path.length - 1].id
+                if (Number.isNaN(path[path.length - 1].id)) newPathName += 'not-found'
+                else newPathName += path[path.length - 1].id
 
+            } else newPathName += '/home'
         }
-        else newPathName += '/home'
 
         navigate(newPathName)
 
