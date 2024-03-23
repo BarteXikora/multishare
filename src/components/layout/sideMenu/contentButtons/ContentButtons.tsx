f419fd5294659f63f9880861fc1a3e767e990d4b
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-23 11:29:02 +0100

    Fixed the files icon in the SideMenu component.

--- src/components/layout/sideMenu/contentButtons/ContentButtons.tsx
+++ src/components/layout/sideMenu/contentButtons/ContentButtons.tsx
@@ -1,7 +1,7 @@
 import Button from '../../../ui/button/Button'
 
 import iconFolder from '../../../../assets/icons/icon-folder.svg'
-import iconFiles from '../../../../assets/icons/icon-files.svg'
+import iconFiles from '../../../../assets/icons/icon-files-full.svg'
 import iconStar from '../../../../assets/icons/icon-star.svg'
 import iconTrash from '../../../../assets/icons/icon-trash.svg'
 
