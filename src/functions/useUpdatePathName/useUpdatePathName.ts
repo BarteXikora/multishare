bcea22b1eae72aaaae8a48e6f6ec93916c1f756f
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-24 09:34:25 +0100

    Added handling displayType changing to the useUpdatePathName hook.

--- src/functions/useUpdatePathName/useUpdatePathName.ts
+++ src/functions/useUpdatePathName/useUpdatePathName.ts
@@ -1,9 +1,12 @@
 import { useEffect } from 'react'
-import { useSelector } from '../../store/store'
-import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from '../../store/store'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { setDisplayType, setTreeLocation } from '../../store/features/contentSlice/contentSlice'
 
 const useUpdatePathName = () => {
     const navigate = useNavigate()
+    const location = useLocation()
+    const dispatch = useDispatch()
 
     const currentFolder = useSelector(state => state.content.currentFolder)
     const project = useSelector(state => state.project.selectedProject)
@@ -29,6 +32,17 @@ const useUpdatePathName = () => {
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [currentFolder])
+
+    useEffect(() => {
+        const pathType = /\/files/.test(location.pathname) ? 'FILES' : 'TREE'
+
+        if (displayType !== pathType) {
+            dispatch(setDisplayType(pathType))
+            dispatch(setTreeLocation(-1))
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [location.pathname])
 }
 
 export default useUpdatePathName
