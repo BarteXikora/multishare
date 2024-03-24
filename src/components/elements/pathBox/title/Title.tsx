44bc8ad5b74c3b4b6ea7fc7efe7c149a97728ccb
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-24 18:16:55 +0100

    Added the trash title functionality to the PathBox component.

--- src/components/elements/pathBox/title/Title.tsx
+++ src/components/elements/pathBox/title/Title.tsx
@@ -1,12 +1,26 @@
 import StyledTitle from './Title.styles'
+import { displayTypeType } from '../../../../store/features/contentSlice/contentSlice.types'
 
 import iconFiles from '../../../../assets/icons/icon-files.svg'
+import iconTrash from '../../../../assets/icons/icon-trash.svg'
 
-const Title = () => {
+const Title = ({ displayType }: { displayType: displayTypeType }) => {
     return <StyledTitle>
-        <img src={iconFiles} alt='Wszystkie pliki' />
+        {
+            displayType === 'FILES' && <>
+                <img src={iconFiles} alt='Wszystkie pliki' />
 
-        <h2>Wszystkie pliki:</h2>
+                <h2>Wszystkie pliki:</h2>
+            </>
+        }
+
+        {
+            displayType === 'TRASH' && <>
+                <img src={iconTrash} alt='Kosz' />
+
+                <h2>Kosz:</h2>
+            </>
+        }
     </StyledTitle>
 }
 
