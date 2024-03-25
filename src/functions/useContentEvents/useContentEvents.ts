cf961cbb07a8f87fea4ed8d22c7665e36981d1e7
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-25 15:54:14 +0100

    Moved the mobile events functions from the useContentEvents hook to the separate hook.

--- src/functions/useContentEvents/useContentEvents.ts
+++ src/functions/useContentEvents/useContentEvents.ts
@@ -1,27 +1,16 @@
-import { useState } from 'react'
 import { useSelector, useDispatch } from '../../store/store'
 import { setSelected } from '../../store/features/contentSlice/contentSlice'
 import { useNavigate } from 'react-router-dom'
 
-import click from './click/click'
-import controlClick from './controlClick/controlClick'
 import selectAllClick from './selectAllClick/selectAllClick'
 
 import useSelect from './useSelect/useSelect'
 import useOpenFolder from './useOpenFolder/useOpenFolder'
+import useMobileEvents from './useMobileEvents/useMobileEvents'
 
-import { elementType, selectedType } from '../../store/features/contentSlice/contentSlice.types'
+import { selectedType } from '../../store/features/contentSlice/contentSlice.types'
 
 const emptySelect: selectedType = { folders: [], files: [], selectionStart: null }
-
-const selectedCnt = (selected: selectedType): number => {
-    let cnt = 0
-
-    if (selected.folders) cnt += selected.folders.length
-    if (selected.files) cnt += selected.files.length
-
-    return cnt
-}
 
 const useContentEvents = () => {
     const currentFolder = useSelector(state => state.content.currentFolder)
@@ -29,34 +18,9 @@ const useContentEvents = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
-    const [touchHoldTimeout, setTouchHoldTimeout] = useState<ReturnType<typeof setTimeout> | null>(null)
-
     const select = useSelect()
     const openFolder = useOpenFolder()
-
-    const selectMobile = (event: React.TouchEvent<HTMLElement>, isTouchStart: boolean, type: elementType, id: number) => {
-        event.preventDefault()
-
-        if (isTouchStart) setTouchHoldTimeout(setTimeout(() => {
-            if (selectedCnt(selected) === 0) dispatch(setSelected(click(type, id)))
-
-            if (touchHoldTimeout) clearTimeout(touchHoldTimeout)
-            setTouchHoldTimeout(null)
-
-        }, 500))
-
-        else {
-            if (touchHoldTimeout !== null) {
-                clearTimeout(touchHoldTimeout)
-                setTouchHoldTimeout(null)
-
-                if (selectedCnt(selected) === 0) {
-                    if (type === 'FOLDER') openFolder(id)
-                }
-                else dispatch(setSelected(controlClick({ ...selected }, type, id)))
-            }
-        }
-    }
+    const mobileEvents = useMobileEvents()
 
     const selectAll = (unselectAllOnSecondClick: boolean = true) => dispatch(
         setSelected(selectAllClick(currentFolder, selected, unselectAllOnSecondClick))
@@ -68,15 +32,15 @@ const useContentEvents = () => {
         folderEvents: {
             onClick: (event: React.MouseEvent<HTMLElement>, folderId: number) => select(event, 'FOLDER', folderId),
             onDoubleClick: (folderId: number) => openFolder(folderId),
-            onTouchStart: (event: React.TouchEvent<HTMLElement>, folderId: number) => selectMobile(event, true, 'FOLDER', folderId),
-            onTouchEnd: (event: React.TouchEvent<HTMLElement>, folderId: number) => selectMobile(event, false, 'FOLDER', folderId)
+            onTouchStart: (event: React.TouchEvent<HTMLElement>, folderId: number) => mobileEvents(event, true, 'FOLDER', folderId),
+            onTouchEnd: (event: React.TouchEvent<HTMLElement>, folderId: number) => mobileEvents(event, false, 'FOLDER', folderId)
         },
 
         filesEvents: {
             onClick: (event: React.MouseEvent<HTMLElement>, fileId: number) => select(event, 'FILE', fileId),
             onDoubleClick: (fileId: number) => navigate('/file/' + fileId.toString()),
-            onTouchStart: (event: React.TouchEvent<HTMLElement>, fileId: number) => selectMobile(event, true, 'FILE', fileId),
-            onTouchEnd: (event: React.TouchEvent<HTMLElement>, fileId: number) => selectMobile(event, false, 'FILE', fileId)
+            onTouchStart: (event: React.TouchEvent<HTMLElement>, fileId: number) => mobileEvents(event, true, 'FILE', fileId),
+            onTouchEnd: (event: React.TouchEvent<HTMLElement>, fileId: number) => mobileEvents(event, false, 'FILE', fileId)
         },
 
         selectAll,
---
+++ src/functions/useContentEvents/useMobileEvents/useMobileEvents.ts
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { useSelector, useDispatch } from '../../../store/store'
+import { setSelected } from '../../../store/features/contentSlice/contentSlice'
+import useOpenFolder from '../useOpenFolder/useOpenFolder'
+import click from '../click/click'
+import controlClick from '../controlClick/controlClick'
+
+import { selectedType, elementType } from '../../../store/features/contentSlice/contentSlice.types'
+
+const useMobileEvents = () => {
+    const dispatch = useDispatch()
+    const openFolder = useOpenFolder()
+
+    const selected = useSelector(state => state.content.selected)
+
+    const [touchHoldTimeout, setTouchHoldTimeout] = useState<ReturnType<typeof setTimeout> | null>(null)
+
+    const selectedCnt = (selected: selectedType): number => {
+        let cnt = 0
+
+        if (selected.folders) cnt += selected.folders.length
+        if (selected.files) cnt += selected.files.length
+
+        return cnt
+    }
+
+    const mobileEvents = (event: React.TouchEvent<HTMLElement>, isTouchStart: boolean, type: elementType, id: number) => {
+        event.preventDefault()
+
+        if (isTouchStart) setTouchHoldTimeout(setTimeout(() => {
+            if (selectedCnt(selected) === 0) dispatch(setSelected(click(type, id)))
+
+            if (touchHoldTimeout) clearTimeout(touchHoldTimeout)
+            setTouchHoldTimeout(null)
+
+        }, 500))
+
+        else {
+            if (touchHoldTimeout !== null) {
+                clearTimeout(touchHoldTimeout)
+                setTouchHoldTimeout(null)
+
+                if (selectedCnt(selected) === 0) {
+                    if (type === 'FOLDER') openFolder(id)
+                }
+                else dispatch(setSelected(controlClick({ ...selected }, type, id)))
+            }
+        }
+    }
+
+    return mobileEvents
+}
+
+export default useMobileEvents
