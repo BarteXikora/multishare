9dba1c2b724b586aae5eb5de79c31148a98ec3ba
Bartłomiej Sikora <sbartek50@gmail.com>
2024-03-25 17:13:38 +0100

    Added functionality of showing warning window on open folders and files in the trash.

--- src/functions/useContentEvents/useOpenFile/useOpenFile.ts
+++ src/functions/useContentEvents/useOpenFile/useOpenFile.ts
@@ -1,9 +1,16 @@
+import { useSelector, useDispatch } from '../../../store/store'
 import { useNavigate } from 'react-router-dom'
+import { showWindow } from '../../../store/features/windowSlice/windowSlice'
 
 const useOpenFile = () => {
+    const dispatch = useDispatch()
     const navigate = useNavigate()
 
+    const isInTrash = useSelector(state => state.content.displayType) === 'TRASH'
+
     const openFile = (id: number) => {
+        if (isInTrash) return dispatch(showWindow({ title: 'Element znajduje się w koszu', content: 'CAN_NOT_OPEN_IN_TRASH' }))
+
         navigate('/file/' + id.toString())
     }
 
--- src/functions/useContentEvents/useOpenFolder/useOpenFolder.ts
+++ src/functions/useContentEvents/useOpenFolder/useOpenFolder.ts
@@ -1,10 +1,17 @@
-import { useDispatch } from '../../../store/store'
+import { useSelector, useDispatch } from '../../../store/store'
 import { setTreeLocation } from '../../../store/features/contentSlice/contentSlice'
+import { showWindow } from '../../../store/features/windowSlice/windowSlice'
 
 const useOpenFolder = () => {
     const dispatch = useDispatch()
 
-    const openFolder = (folderId: number) => dispatch(setTreeLocation(folderId))
+    const isInTrash = useSelector(state => state.content.displayType) === 'TRASH'
+
+    const openFolder = (folderId: number) => {
+        if (isInTrash) return dispatch(showWindow({ title: 'Element znajduje się w koszu', content: 'CAN_NOT_OPEN_IN_TRASH' }))
+
+        dispatch(setTreeLocation(folderId))
+    }
 
     return openFolder
 }
