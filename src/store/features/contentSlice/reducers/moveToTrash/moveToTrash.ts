9d1fa1abba844d94f9532cdbe00518968ad55406
Bartłomiej Sikora <sbartek50@gmail.com>
2024-04-23 14:29:08 +0200

    Created the new version of the moveToTrash reducer.

--- src/store/features/contentSlice/reducers/moveToTrash/moveToTrash.ts
+++ src/store/features/contentSlice/reducers/moveToTrash/moveToTrash.ts
@@ -1,14 +1,54 @@
 import { PayloadAction } from '@reduxjs/toolkit'
 import { contentStateType } from '../../contentSlice.types'
 
-const moveToTrash = (state: contentStateType, action: PayloadAction<{ folders: number[], files: number[] }>) => {
+type moveToTrashType = {
+    view: { folders: number[], files: number[] },
+    contained: { folders: number[], files: number[] }
+}
+
+const moveToTrash = (state: contentStateType, action: PayloadAction<moveToTrashType>) => {
     if (state.loadedContent.status !== 'READY') return
 
-    state.loadedContent.content.folders
-        = state.loadedContent.content.folders.map(f => action.payload.folders.includes(f.id) ? { ...f, isInTrash: true } : f)
+    const data = action.payload
+    let currentContent = { ...state.loadedContent }
 
-    state.loadedContent.content.files
-        = state.loadedContent.content.files.map(f => action.payload.files.includes(f.id) ? { ...f, isInTrash: true } : f)
+    data.view.folders.forEach(folderId => {
+        const folder = currentContent.content.folders.find(f => f.id === folderId)
+
+        if (folder) {
+            currentContent.content.folders.splice(currentContent.content.folders.indexOf(folder), 1)
+            currentContent.trash.view.folders.push(folder)
+        }
+    })
+
+    data.view.files.forEach(fileId => {
+        const file = currentContent.content.files.find(f => f.id === fileId)
+
+        if (file) {
+            currentContent.content.files.splice(currentContent.content.files.indexOf(file), 1)
+            currentContent.trash.view.files.push(file)
+        }
+    })
+
+    data.contained.folders.forEach(folderId => {
+        const folder = currentContent.content.folders.find(f => f.id === folderId)
+
+        if (folder) {
+            currentContent.content.folders.splice(currentContent.content.folders.indexOf(folder), 1)
+            currentContent.trash.contained.folders.push(folder)
+        }
+    })
+
+    data.contained.files.forEach(fileId => {
+        const file = currentContent.content.files.find(f => f.id === fileId)
+
+        if (file) {
+            currentContent.content.files.splice(currentContent.content.files.indexOf(file), 1)
+            currentContent.trash.contained.files.push(file)
+        }
+    })
+
+    state.loadedContent = { ...currentContent }
 }
 
 export default moveToTrash
