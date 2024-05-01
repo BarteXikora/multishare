98bc65e15f6f5dbe1269e883d6600520ba046d31
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-01 23:01:42 +0200

    Created the restore from trash functionality on the mocked server.

--- src/mock/server/index.js
+++ src/mock/server/index.js
@@ -207,6 +207,124 @@ io.on('connection', (socket) => {
 
         socket.to(room).emit('deleted_forever', data)
     })
+
+    socket.on('restore_from_trash', data => {
+        data = {
+            folders: data.folders.map(f => f.id),
+            files: data.files.map(f => f.id)
+        }
+
+        console.log('restore_from_trash', data)
+
+        const room = socket.rooms.has(0) ? 0 : socket.rooms.has(1) ? 1 : -1
+        if (room === -1) return socket.emit('restored_from_trash', null)
+
+        let currentContent = room === 0 ? { ...contentDefault } : { ...contentProject1 }
+
+        // console.log('\n\n before: ', util.inspect(currentContent, false, null, true))
+
+        const allFolders = [
+            ...currentContent.content.folders,
+            ...currentContent.trash.view.folders,
+            ...currentContent.trash.contained.folders
+        ]
+
+        let foldersBuffer = []
+
+        data.folders.forEach(folderId => {
+            const found = currentContent.trash.view.folders.find(f => f.id === folderId)
+
+            if (found) foldersBuffer.push(found)
+        })
+
+        currentContent.trash.contained.folders.forEach(folder => {
+            const path = getPath(allFolders, folder.id)
+            path.shift()
+
+            // console.log('\n\n', folder.name, 'whole path: ', path)
+
+            let isToRestore = false
+
+            data.folders.forEach(folderId => {
+                if (!path.includes(folderId)) return
+
+                let indexOfCurrent = path.indexOf(folderId)
+                path.splice(indexOfCurrent, path.length - indexOfCurrent)
+
+                // console.log(folder.name, 'path to current', path)
+
+                let includesParentInTrash = false
+                path.forEach(element => {
+                    if (currentContent.trash.view.folders.find(f => f.id === element)) includesParentInTrash = true
+                })
+
+                // console.log('includesParentInTrash:', includesParentInTrash)
+
+                if (!includesParentInTrash) isToRestore = true
+            })
+
+            if (isToRestore) foldersBuffer.push(folder)
+        })
+
+        // console.log('\n\nfolders effect: ', foldersBuffer, '\n\n')
+
+        let filesBuffer = []
+
+        data.files.forEach(fileId => {
+            const found = currentContent.trash.view.files.find(f => f.id === fileId)
+
+            if (found) filesBuffer.push(found)
+        })
+
+        currentContent.trash.contained.files.forEach(file => {
+            if (foldersBuffer.find(f => f.id === file.parentFolder)) filesBuffer.push(file)
+        })
+
+        // console.log('files effect', filesBuffer)
+
+        // ====================================================================
+
+        let response = { folders: [], files: [] }
+
+        foldersBuffer.forEach(folder => {
+            let found = currentContent.trash.view.folders.find(f => f.id === folder.id)
+
+            if (found) currentContent.trash.view.folders.splice(currentContent.trash.view.folders.indexOf(found), 1)
+            else {
+                found = currentContent.trash.contained.folders.find(f => f.id === folder.id)
+
+                if (found) currentContent.trash.contained.folders.splice(currentContent.trash.contained.folders.indexOf(found), 1)
+            }
+
+            if (![...currentContent.content.folders, ...foldersBuffer].find(f => f.id === folder.parentFolder)) folder.parentFolder = -1
+            currentContent.content.folders.push(folder)
+
+            response.folders.push({ id: folder.id, parentFolder: folder.parentFolder })
+        })
+
+        filesBuffer.forEach(file => {
+            let found = currentContent.trash.view.files.find(f => f.id === file.id)
+
+            if (found) currentContent.trash.view.files.splice(currentContent.trash.view.files.indexOf(found), 1)
+            else {
+                found = currentContent.trash.contained.files.find(f => f.id === file.id)
+
+                if (found) currentContent.trash.contained.files.splice(currentContent.trash.contained.files.indexOf(found), 1)
+            }
+
+            if (![...currentContent.content.folders, ...foldersBuffer].find(f => f.id === file.parentFolder)) file.parentFolder = -1
+            currentContent.content.files.push(file)
+
+            response.files.push({ id: file.id, parentFolder: file.parentFolder })
+        })
+
+        // console.log('\n\n after: ', util.inspect(currentContent, false, null, true))
+
+        if (room === 0) contentDefault = { ...currentContent }
+        else contentProject1 = { ...currentContent }
+
+        socket.to(room).emit('restored_from_trash', response)
+    })
 })
 
 server.listen(3001, () => console.log('server is listening on port 3001'))
