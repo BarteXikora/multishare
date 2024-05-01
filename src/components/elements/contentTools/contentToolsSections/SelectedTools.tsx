1aaa08012f173b3200f91dd7d2f4ed5aff9214ad
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-01 22:59:44 +0200

    Added functionality to the restore from trash button in the contentTools component.

--- src/components/elements/contentTools/contentToolsSections/SelectedTools.tsx
+++ src/components/elements/contentTools/contentToolsSections/SelectedTools.tsx
@@ -2,6 +2,8 @@ import Button from '../../../ui/button/Button'
 import { useDispatch, useSelector } from '../../../../store/store'
 import { toggle } from '../../../../store/features/detailsSectionSlice/detailsSectionSlice'
 import { showWindow } from '../../../../store/features/windowSlice/windowSlice'
+import { restoreFromTrash } from '../../../../store/features/contentSlice/contentSlice'
+import { restoreFromTrashType } from '../../../../store/features/contentSlice/reducers/restoreFromTrash/restorFromTrash'
 
 import iconDownload from '../../../../assets/icons/icon-download.svg'
 import iconMove from '../../../../assets/icons/icon-move.svg'
@@ -12,6 +14,8 @@ import iconDetails from '../../../../assets/icons/icon-details.svg'
 
 const SelectedTools = () => {
     const dispatch = useDispatch()
+
+    const selected = useSelector(state => state.content.selected)
     const displayType = useSelector(state => state.content.displayType)
 
     const handleDelete = () => {
@@ -19,6 +23,15 @@ const SelectedTools = () => {
             title: 'Czy na pewno chcesz przenieść wybrane elementy do kosza?',
             content: 'CONFIRM_DELETE'
         }))
+    }
+
+    const handleRestoreFromTrash = () => {
+        const data: restoreFromTrashType = {
+            folders: selected.folders.map(f => { return { id: f, parentFolder: 0 } }),
+            files: selected.files.map(f => { return { id: f, parentFolder: 0 } })
+        }
+
+        dispatch(restoreFromTrash(data))
     }
 
     const handleDeleteForever = () => {
@@ -59,7 +72,7 @@ const SelectedTools = () => {
 
         {
             displayType === 'TRASH' && <div className="tools-buttons">
-                <Button>
+                <Button onClick={handleRestoreFromTrash}>
                     <img src={iconRestore} alt="Przywróć..." />
 
                     <span className="label">Przywróć...</span>
