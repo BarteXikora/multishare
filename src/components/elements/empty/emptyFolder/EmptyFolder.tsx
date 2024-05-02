a66e2e68d2ae7d295c6418c4b669ddf576f5f14a
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-02 20:18:16 +0200

    Moved the content from the Empty component to the EmpyFolder component.

---
+++ src/components/elements/empty/emptyFolder/EmptyFolder.tsx
@@ -0,0 +1,36 @@
+import Button from '../../../ui/button/Button'
+import useGoBack from '../../../../functions/useGoBack/useGoBack'
+
+import imageEmptyFolder from '../../../../assets/images/img-empty-folder.svg'
+import iconBack from '../../../../assets/icons/icon-back.svg'
+import iconUpload from '../../../../assets/icons/icon-upload.svg'
+
+const EmptyFolder = () => {
+    const goBack = useGoBack()
+
+    return <>
+        <div className="image">
+            <img src={imageEmptyFolder} alt="" />
+        </div>
+
+        <div className="info">
+            <h2>Ten folder jest pusty!</h2>
+
+            <p>Możesz przesłać pliki na dysk poprzez przeciągnięcie i opuszczenie je na okno przeglądarki.</p>
+
+            <div className="actions">
+                <Button $variant='secondary' className='back-button' onClick={() => goBack()}>
+                    <img src={iconBack} alt='Cofnij się' />
+                </Button>
+
+                <Button>
+                    <img src={iconUpload} alt='Prześlij pliki tutaj' />
+
+                    Prześlij pliki tutaj
+                </Button>
+            </div>
+        </div>
+    </>
+}
+
+export default EmptyFolder
