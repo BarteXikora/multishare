0a074b64d81349055fda3537715fa0df2a7012cf
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-02 20:25:18 +0200

    Added conditional rendering of the EmptyFolder component.

--- src/components/elements/empty/Empty.tsx
+++ src/components/elements/empty/Empty.tsx
@@ -1,10 +1,14 @@
 import StyledEmpty from './Empty.styles'
+import { useSelector } from '../../../store/store'
 
 import EmptyFolder from './emptyFolder/EmptyFolder'
 
 const Empty = () => {
+    const displayType = useSelector(state => state.content.displayType)
+    const isHome = useSelector(state => state.content.currentPath.length === 0)
+
     return <StyledEmpty>
-        <EmptyFolder />
+        {(displayType === 'TREE' && !isHome) && <EmptyFolder />}
 
     </StyledEmpty>
 }
