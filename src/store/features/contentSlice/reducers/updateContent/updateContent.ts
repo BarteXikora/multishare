d447bdac3dc3d0169405400cc497a4504ce78202
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-08 17:00:12 +0200

    Prepared the updateContent reducer.

---
+++ src/store/features/contentSlice/reducers/updateContent/updateContent.ts
@@ -0,0 +1,6 @@
+import { PayloadAction } from '@reduxjs/toolkit'
+import { contentStateType, updateContentType } from '../../contentSlice.types'
+
+const updateContent = (state: contentStateType, action: PayloadAction<updateContentType>) => { }
+
+export default updateContent
