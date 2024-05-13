28fe3ddbcda2d02339318c37744c1eea0a41acac
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-13 16:58:31 +0200

    Prepared the downloadElements reducer.

---
+++ src/store/features/contentSlice/reducers/downloadElements/downloadElements.ts
@@ -0,0 +1,19 @@
+import { PayloadAction } from '@reduxjs/toolkit'
+import { contentStateType } from '../../contentSlice.types'
+
+type downloadElemetsRequestType = {
+    type: 'REQ',
+    folders: number[],
+    files: number[]
+}
+
+type downloadElemetsResponseType = {
+    type: 'RES',
+    data: string
+}
+
+type downloadElemetsType = downloadElemetsRequestType | downloadElemetsResponseType
+
+const downloadElements = (state: contentStateType, action: PayloadAction<downloadElemetsType>) => { }
+
+export default downloadElements
