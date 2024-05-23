0615684ad36fced913930f1e4e1a48d46e658b2b
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-23 14:16:46 +0200

    Created the uploadFiles reducer.

--- src/store/features/contentSlice/reducers/uploadFiles/uploadFiles.ts
+++ src/store/features/contentSlice/reducers/uploadFiles/uploadFiles.ts
@@ -14,6 +14,13 @@ type uploadFilesRequestType = {
 
 type uploadFilesType = uploadFilesRequestType | fileType[]
 
-const uploadFiles = (state: contentStateType, action: PayloadAction<uploadFilesType>) => { }
+const uploadFiles = (state: contentStateType, action: PayloadAction<uploadFilesType>) => {
+    if (state.loadedContent.status !== 'READY') return
+    if ('location' in action.payload) return
+
+    let newFiles = [...state.loadedContent.content.files, ...action.payload]
+
+    state.loadedContent.content.files = [...newFiles]
+}
 
 export default uploadFiles
