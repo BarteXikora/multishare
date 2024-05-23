88a38ac6ebef8ac590183bab8f26a0a31444dc9e
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-23 14:33:14 +0200

    Added functionality to the upload button in the Empty component.

--- src/components/elements/empty/emptyFolder/EmptyFolder.tsx
+++ src/components/elements/empty/emptyFolder/EmptyFolder.tsx
@@ -1,3 +1,6 @@
+import { useDispatch } from '../../../../store/store'
+import { showWindow } from '../../../../store/features/windowSlice/windowSlice'
+
 import Button from '../../../ui/button/Button'
 import useGoBack from '../../../../functions/useGoBack/useGoBack'
 
@@ -6,7 +9,12 @@ import iconBack from '../../../../assets/icons/icon-back.svg'
 import iconUpload from '../../../../assets/icons/icon-upload.svg'
 
 const EmptyFolder = () => {
+    const dispatch = useDispatch()
     const goBack = useGoBack()
+
+    const handleUpload = () => {
+        dispatch(showWindow({ title: 'Prześlij pliki', content: 'UPLOAD' }))
+    }
 
     return <>
         <div className="image">
@@ -23,10 +31,10 @@ const EmptyFolder = () => {
                     <img src={iconBack} alt='Cofnij się' />
                 </Button>
 
-                <Button>
+                <Button onClick={handleUpload}>
                     <img src={iconUpload} alt='Prześlij pliki tutaj' />
 
-                    Prześlij pliki tutaj
+                    Prześlij pliki
                 </Button>
             </div>
         </div>
