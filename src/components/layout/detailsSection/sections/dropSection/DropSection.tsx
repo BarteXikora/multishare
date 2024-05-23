359e4aba9f7d2739f8b099f49f87246fce6ddfe3
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-23 12:56:54 +0200

    Used the DropArea component in the DetailsSection component.

--- src/components/layout/detailsSection/sections/dropSection/DropSection.tsx
+++ src/components/layout/detailsSection/sections/dropSection/DropSection.tsx
@@ -1,14 +1,48 @@
-import StyledDropSection from './DropSection.styles'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from '../../../../../store/store'
+import { uploadFiles } from '../../../../../store/features/contentSlice/contentSlice'
 
-import imgDrop from '../../../../../assets/images/img-drop.svg'
+import StyledDropSection from './DropSection.styles'
+import DropArea from '../../../../elements/dropArea/DropArea'
 
 const DropSection = () => {
-    return <StyledDropSection>
-        <div className="content">
-            <img src={imgDrop} alt='Przeciągnij i upuść pliki tutaj' />
+    const dispatch = useDispatch()
 
-            Upuść pliki i foldery tutaj, aby dodać je na dysk!
-        </div>
+    const currentPath = useSelector(state => state.content.currentPath)
+
+    const [files, setFiles] = useState<FileList | null>(null)
+    const [location, setLocation] = useState<number | null>(null)
+
+    useEffect(() => {
+        const currentLocation = currentPath.length > 0 ? currentPath[currentPath.length - 1].id : -1
+        setLocation(currentLocation)
+
+    }, [currentPath])
+
+    const handleUpload = () => {
+        if (!files || !location) return
+
+        const filesToUpload = Array.from(files).map(f => {
+            return {
+                file: new File([f], f.name),
+                name: f.name,
+                extension: f.name.split('.').pop() || null
+            }
+        })
+
+        dispatch(uploadFiles({ location, data: filesToUpload }))
+
+        setFiles(null)
+    }
+
+    return <StyledDropSection>
+        <DropArea
+            filesState={[files, setFiles]}
+            locationState={[location, setLocation]}
+            upload={handleUpload}
+            showLocationSelector={false}
+            showSelectButton={false}
+        />
     </StyledDropSection>
 }
 
