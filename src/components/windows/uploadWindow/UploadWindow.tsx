7f6886e349f06a869fe3bef6df7e256c9980b568
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-23 12:58:19 +0200

    Used the DropArea component in the UploadWindow.

--- src/components/windows/uploadWindow/UploadWindow.tsx
+++ src/components/windows/uploadWindow/UploadWindow.tsx
@@ -1,7 +1,40 @@
+import { useState, useEffect } from 'react'
+import { useDispatch } from '../../../store/store'
+import { uploadFiles } from '../../../store/features/contentSlice/contentSlice'
+import { closeWindow } from '../../../store/features/windowSlice/windowSlice'
+
 import StyledUploadWindow from './UploadWindow.styles'
+import DropArea from '../../elements/dropArea/DropArea'
 
 const UploadWindow = () => {
-    return <StyledUploadWindow>upload</StyledUploadWindow>
+    const dispatch = useDispatch()
+
+    const [files, setFiles] = useState<FileList | null>(null)
+    const [location, setLocation] = useState<number | null>(null)
+
+    useEffect(() => { if (!files) setLocation(null) }, [files])
+
+    const handleUpload = () => {
+        if (!files || !location) return
+
+        const filesToUpload = Array.from(files).map(f => {
+            return {
+                file: new File([f], f.name),
+                name: f.name,
+                extension: f.name.split('.').pop() || null
+            }
+        })
+
+        dispatch(uploadFiles({ location, data: filesToUpload }))
+
+        setFiles(null)
+
+        dispatch(closeWindow())
+    }
+
+    return <StyledUploadWindow>
+        <DropArea filesState={[files, setFiles]} locationState={[location, setLocation]} upload={handleUpload} />
+    </StyledUploadWindow>
 }
 
 export default UploadWindow
