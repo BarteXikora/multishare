2dfcbd652bbc6bdb88a24e65b374f888f1dde2fd
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-24 20:02:54 +0200

    Created the moveToTrash socket event.

--- src/mock/server/socketEvents/index.js
+++ src/mock/server/socketEvents/index.js
@@ -2,12 +2,14 @@ const getProjects = require('./getProjects')
 const enterProject = require('./enterProject')
 const getFile = require('./getFile')
 const addFolder = require('./addFolder')
+const moveToTrash = require('./moveToTrash')
 
 const socketEvents = [
     { name: 'get_projects', callback: getProjects },
     { name: 'enter_project', callback: enterProject },
     { name: 'get_file', callback: getFile },
-    { name: 'add_folder', callback: addFolder }
+    { name: 'add_folder', callback: addFolder },
+    { name: 'move_to_trash', callback: moveToTrash }
 ]
 
 module.exports = socketEvents
---
+++ src/mock/server/socketEvents/moveToTrash.js
@@ -0,0 +1,70 @@
+const getUserProject = require('../functions/getUserProject')
+const getProjectContent = require('../functions/getProjectContent')
+const setProjestContent = require('../functions/setProjectContent')
+const getPath = require('../functions/getPath')
+
+const moveToTrash = (socket, data) => {
+    const currentProject = getUserProject(socket)
+    const currentContent = getProjectContent(currentProject)
+
+    const allToTrash = { view: { folders: data.folders, files: data.files }, contained: { folders: [], files: [] } }
+
+    allToTrash.view.folders.forEach(element => {
+        currentContent.content.folders.forEach(folder => {
+            if (getPath(currentContent.content.folders, folder.id).includes(element))
+                allToTrash.contained.folders.push(folder.id)
+        })
+
+        currentContent.content.files.forEach(file => {
+            if (getPath(currentContent.content.folders, file.parentFolder).includes(element))
+                allToTrash.contained.files.push(file.id)
+        })
+    })
+
+    allToTrash.contained.folders.forEach(element => {
+        if (allToTrash.view.folders.includes(element))
+            allToTrash.contained.folders.splice(allToTrash.contained.folders.indexOf(element), 1)
+    })
+
+    allToTrash.view.folders.forEach(folderId => {
+        let folder = currentContent.content.folders.find(f => f.id === folderId)
+
+        if (folder) {
+            currentContent.content.folders.splice(currentContent.content.folders.indexOf(folder), 1)
+            currentContent.trash.view.folders.push(folder)
+        }
+    })
+
+    allToTrash.view.files.forEach(fileId => {
+        let file = currentContent.content.files.find(f => f.id === fileId)
+
+        if (file) {
+            currentContent.content.files.splice(currentContent.content.files.indexOf(file), 1)
+            currentContent.trash.view.files.push(file)
+        }
+    })
+
+    allToTrash.contained.folders.forEach(folderId => {
+        let folder = currentContent.content.folders.find(f => f.id === folderId)
+
+        if (folder) {
+            currentContent.content.folders.splice(currentContent.content.folders.indexOf(folder), 1)
+            currentContent.trash.contained.folders.push(folder)
+        }
+    })
+
+    allToTrash.contained.files.forEach(fileId => {
+        let file = currentContent.content.files.find(f => f.id === fileId)
+
+        if (file) {
+            currentContent.content.files.splice(currentContent.content.files.indexOf(file), 1)
+            currentContent.trash.contained.files.push(file)
+        }
+    })
+
+    setProjestContent(currentContent, currentContent)
+
+    socket.to(currentProject).emit('moved_to_trash', allToTrash)
+}
+
+module.exports = moveToTrash
