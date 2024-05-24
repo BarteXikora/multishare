7c6407b73781b26e6530bbf853cf70c8cdd4c05c
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-24 19:23:26 +0200

    Created the enterProject socket event.

--- src/mock/server/socketEvents/index.js
+++ src/mock/server/socketEvents/index.js
@@ -1,7 +1,9 @@
 const getProjects = require('./getProjects')
+const enterProject = require('./enterProject')
 
 const socketEvents = [
-    { name: 'get_projects', callback: getProjects }
+    { name: 'get_projects', callback: getProjects },
+    { name: 'enter_project', callback: enterProject }
 ]
 
 module.exports = socketEvents
