dcb00b7aa3a90912376cf4ce9c3ce4040ca074a6
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-24 20:08:52 +0200

    Created the deleteForever socket event.

--- src/mock/server/socketEvents/index.js
+++ src/mock/server/socketEvents/index.js
@@ -3,13 +3,15 @@ const enterProject = require('./enterProject')
 const getFile = require('./getFile')
 const addFolder = require('./addFolder')
 const moveToTrash = require('./moveToTrash')
+const deleteForever = require('./deleteForever')
 
 const socketEvents = [
     { name: 'get_projects', callback: getProjects },
     { name: 'enter_project', callback: enterProject },
     { name: 'get_file', callback: getFile },
     { name: 'add_folder', callback: addFolder },
-    { name: 'move_to_trash', callback: moveToTrash }
+    { name: 'move_to_trash', callback: moveToTrash },
+    { name: 'delete_forever', callback: deleteForever }
 ]
 
 module.exports = socketEvents
