023b954907d6733942e15fa2c648e497057261be
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-25 16:35:49 +0200

    Added functionality to the SortWindow.

--- src/components/windows/sortWindow/SortWindow.tsx
+++ src/components/windows/sortWindow/SortWindow.tsx
@@ -1,3 +1,9 @@
+import { useState } from 'react'
+import { useSelector, useDispatch } from '../../../store/store'
+import { setSort } from '../../../store/features/contentSlice/contentSlice'
+import { sortType } from '../../../store/features/contentSlice/contentSlice.types'
+import { closeWindow } from '../../../store/features/windowSlice/windowSlice'
+
 import StyledSortWindow from './SortWindow.styles'
 import Button from '../../ui/button/Button'
 
@@ -9,23 +15,40 @@ import iconDown from '../../../assets/icons/icon-down.svg'
 import iconSort from '../../../assets/icons/icon-sort.svg'
 
 const SortWindow = () => {
+    const disptach = useDispatch()
+    const sort = useSelector(state => state.content.sort)
+
+    const [selectedSort, setSelectedSort] = useState<sortType>(sort)
+
     return <StyledSortWindow>
         <section className='sort-section'>
             <h2>Sortuj według:</h2>
 
-            <Button $variant='quaternary'>
+            <Button
+                $variant='quaternary'
+                $active={selectedSort.sortBy === 'NAME'}
+                onClick={() => setSelectedSort({ ...selectedSort, sortBy: 'NAME' })}
+            >
                 <img src={iconSortAlphabet} alt="Sortuj Nazwa" />
 
                 Nazwa
             </Button>
 
-            <Button $variant='quaternary'>
+            <Button
+                $variant='quaternary'
+                $active={selectedSort.sortBy === 'DATE'}
+                onClick={() => setSelectedSort({ ...selectedSort, sortBy: 'DATE' })}
+            >
                 <img src={iconSortDate} alt="Data" />
 
                 Data
             </Button>
 
-            <Button $variant='quaternary'>
+            <Button
+                $variant='quaternary'
+                $active={selectedSort.sortBy === 'TYPE'}
+                onClick={() => setSelectedSort({ ...selectedSort, sortBy: 'TYPE' })}
+            >
                 <img src={iconSortType} alt="Typ" />
 
                 Typ
@@ -35,13 +58,21 @@ const SortWindow = () => {
         <section className='sort-section'>
             <h2>Metoda sortowania:</h2>
 
-            <Button $variant='quaternary'>
+            <Button
+                $variant='quaternary'
+                $active={selectedSort.method === 'ASC'}
+                onClick={() => setSelectedSort({ ...selectedSort, method: 'ASC' })}
+            >
                 <img src={iconDown} alt="Rosnąco" />
 
                 Rosnąco
             </Button>
 
-            <Button $variant='quaternary'>
+            <Button
+                $variant='quaternary'
+                $active={selectedSort.method === 'DESC'}
+                onClick={() => setSelectedSort({ ...selectedSort, method: 'DESC' })}
+            >
                 <img src={iconUp} alt="Malejąco" />
 
                 Malejąco
@@ -49,11 +80,11 @@ const SortWindow = () => {
         </section>
 
         <section className="actions">
-            <Button $variant='secondary'>
+            <Button $variant='secondary' onClick={() => disptach(closeWindow())}>
                 Anuluj
             </Button>
 
-            <Button>
+            <Button onClick={() => { disptach(setSort(selectedSort)); disptach(closeWindow()) }}>
                 <img src={iconSort} alt="Sortuj" />
 
                 Sortuj
