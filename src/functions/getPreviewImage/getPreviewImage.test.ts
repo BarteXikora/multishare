271dd8fffac33f994d287b977715f538b5e1195d
Bartłomiej Sikora <sbartek50@gmail.com>
2024-05-27 14:29:29 +0200

    Removed the old getPreviewImage function.

--- src/functions/getPreviewImage/getPreviewImage.test.ts
+++
@@ -1,9 +0,0 @@
-import getPreviewImage from './getPreviewImage'
-
-describe('getPreviewImage function', () => {
-
-    test('returns string for an empty extension input', () => {
-        expect(getPreviewImage(false, '').split('').length).toBeGreaterThan(5)
-    })
-
-})
