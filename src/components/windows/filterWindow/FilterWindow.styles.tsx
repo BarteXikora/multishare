2faedfc4844d106a520963704a65bbf4ac29e7b9
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-05 20:50:43 +0200

    Created the FilterWindow.

--- src/components/windows/filterWindow/FilterWindow.styles.tsx
+++ src/components/windows/filterWindow/FilterWindow.styles.tsx
@@ -1,7 +1,16 @@
 import styled from 'styled-components'
 
 const StyledFilterWindow = styled.div`
-    
+    .main button {
+        width: 100%;
+    }
+
+    .actions {
+        display: flex;
+        justify-content: end;
+        gap: ${(props) => props.theme.margins.medium};
+        margin-top: ${(props) => props.theme.margins.big};
+    }
 `
 
 export default StyledFilterWindow
