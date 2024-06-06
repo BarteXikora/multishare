f7f55291b9a12c34c2ff41aa4ce9fbce8c432735
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-06 19:15:33 +0200

    Moved the DisplayDropdown from the ListTools component to the separate component.

--- src/components/elements/contentTools/contentToolsSections/ListTools.tsx
+++ src/components/elements/contentTools/contentToolsSections/ListTools.tsx
@@ -1,22 +1,13 @@
-import { useSelector, useDispatch } from '../../../../store/store'
-import { setContentViewStyle } from '../../../../store/features/viewSlice/viewSlice'
-
 import Button from '../../../ui/button/Button'
-import Dropdown from '../../../ui/dropdown/Dropdown'
 import SortDropdown from '../elements/SortDropdown'
 import FilterDropdown from '../elements/FilterDropdown'
+import DisplayDropdown from '../elements/DisplayDropdown'
 
 import useContentEvents from '../../../../functions/useContentEvents/useContentEvents'
 
 import iconSelect from '../../../../assets/icons/icon-ok.svg'
-import iconDisplay from '../../../../assets/icons/icon-display.svg'
-import iconList from '../../../../assets/icons/icon-list.svg'
 
 const ListTools = () => {
-    const dispatch = useDispatch()
-
-    const viewStyle = useSelector(state => state.view.contentViewStyle)
-
     const { selectAll } = useContentEvents()
 
     return <section className="list-tools">
@@ -30,32 +21,7 @@ const ListTools = () => {
 
         <FilterDropdown />
 
-        <Dropdown
-            className='button-dropdown'
-            buttonOptions={{ $variant: 'secondary' }}
-            showArrow={false}
-            buttonContent={<>
-                <img src={viewStyle === 'ICONS' ? iconDisplay : iconList} alt="Wyświetl..." />
-
-                Wyświetl...
-            </>}
-
-            dropdownContent={<>
-                <h2>Wyświetl jako:</h2>
-
-                <Button $variant='quaternary' $active={viewStyle === 'ICONS'} onClick={() => dispatch(setContentViewStyle('ICONS'))}>
-                    <img src={iconDisplay} alt="Ikony" />
-
-                    Ikony
-                </Button>
-
-                <Button $variant='quaternary' $active={viewStyle === 'LIST'} onClick={() => dispatch(setContentViewStyle('LIST'))}>
-                    <img src={iconList} alt="Lista" />
-
-                    Lista
-                </Button>
-            </>}
-        />
+        <DisplayDropdown />
     </section>
 }
 
