7ce21e8b837ec96894987e4919e74f4e717d9825
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-06 17:50:00 +0200

    Added functionality to the CanNotOpenInTrashWindow.

--- src/components/windows/canNotOpenInTrashWindow/CanNotOpenInTrashWindow.tsx
+++ src/components/windows/canNotOpenInTrashWindow/CanNotOpenInTrashWindow.tsx
@@ -1,8 +1,27 @@
+import { useSelector, useDispatch } from '../../../store/store'
+import { restoreFromTrash } from '../../../store/features/contentSlice/contentSlice'
+import { closeWindow } from '../../../store/features/windowSlice/windowSlice'
+import { restoreFromTrashType } from '../../../store/features/contentSlice/reducers/restoreFromTrash/restoreFromTrash'
+
 import Button from '../../ui/button/Button'
 
 import iconRestore from '../../../assets/icons/icon-restore.svg'
 
 const CanNotOpenInTrashWindow = () => {
+    const dispatch = useDispatch()
+
+    const selected = useSelector(state => state.content.selected)
+
+    const handleRestoreFromTrash = () => {
+        const data: restoreFromTrashType = {
+            folders: selected.folders.map(f => { return { id: f, parentFolder: 0 } }),
+            files: selected.files.map(f => { return { id: f, parentFolder: 0 } })
+        }
+
+        dispatch(restoreFromTrash(data))
+        dispatch(closeWindow())
+    }
+
     return <>
         <section>
             <h2>Nie można wyświetlić tego elementu, gdyż znajduje się on w koszu.</h2>
@@ -11,11 +30,11 @@ const CanNotOpenInTrashWindow = () => {
         </section>
 
         <section className="actions">
-            <Button $variant='secondary'>
+            <Button $variant='secondary' onClick={() => dispatch(closeWindow())}>
                 Anuluj
             </Button>
 
-            <Button>
+            <Button onClick={handleRestoreFromTrash}>
                 <img src={iconRestore} alt='Przywróć' />
 
                 Przywróć ten element
