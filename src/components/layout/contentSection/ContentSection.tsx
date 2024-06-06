405f8c2cf3c89f7d72089443696bf7ba4bbc6448
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-06 13:46:22 +0200

    Placed the SearchInfo component in the ContentSection component.

--- src/components/layout/contentSection/ContentSection.tsx
+++ src/components/layout/contentSection/ContentSection.tsx
@@ -1,13 +1,17 @@
 import { useSelector } from '../../../store/store'
 
 import StyledContentSection from './ContentSection.styles'
+import SearchInfo from '../../elements/searchInfo/SearchInfo'
 import ContentIconsView from './contentIconsView/ContentIconsView'
 import ContentListView from './contentListView/ContentListView'
 
 const ContentSection = () => {
     const contentViewStyle = useSelector(state => state.view.contentViewStyle)
+    const search = useSelector(state => state.content.search)
 
     return <StyledContentSection>
+        {search !== '' && <SearchInfo search={search} />}
+
         {
             contentViewStyle === 'ICONS' ?
                 <ContentIconsView />
