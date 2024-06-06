1f1ce8acf07588eaa7c64aa00a94e1f4d665ac9f
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-06 11:32:11 +0200

    Added functionality to the mobile search button.

--- src/components/elements/searchInput/SearchInput.tsx
+++ src/components/elements/searchInput/SearchInput.tsx
@@ -1,6 +1,10 @@
+import { useDispatch } from '../../../store/store'
+import { showWindow } from '../../../store/features/windowSlice/windowSlice'
+
 import StyledSeachInput from './SearchInput.styles'
 import Input from '../../ui/input/Input'
 import Button from '../../ui/button/Button'
+
 import iconSearch from '../../../assets/icons/icon-search.svg'
 
 type SearchInputType = {
@@ -8,6 +12,10 @@ type SearchInputType = {
 }
 
 const SearchInput = ({ placeholder }: SearchInputType) => {
+    const dispatch = useDispatch()
+
+    const handleSearchWindow = () => dispatch(showWindow({ title: 'Szukaj', content: 'SEARCH' }))
+
     return <StyledSeachInput>
         <Input placeholder={placeholder || ''} />
 
@@ -15,7 +23,7 @@ const SearchInput = ({ placeholder }: SearchInputType) => {
             <img src={iconSearch} alt="Szukaj" />
         </Button>
 
-        <Button className='open-search-button' $variant='secondary' $size='big'>
+        <Button className='open-search-button' $variant='secondary' $size='big' type='button' onClick={handleSearchWindow}>
             <img src={iconSearch} alt="Szukaj" />
         </Button>
     </StyledSeachInput>
