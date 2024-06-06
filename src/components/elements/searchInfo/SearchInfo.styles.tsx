53c62584e7bd630706ab3d673ba085fceb3dac57
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-06 13:45:47 +0200

    Created the SearchInfo component.

---
+++ src/components/elements/searchInfo/SearchInfo.styles.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const StyledSearchInfo = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: ${(props) => props.theme.margins.medium};
+    margin: ${(props) => props.theme.margins.medium + ' 0 ' + props.theme.margins.big + ' 0'};
+    padding: ${(props) => props.theme.margins.sectionMedium};
+    background-color: ${(props) => props.theme.colors.primary2};
+    border-radius: ${(props) => props.theme.borderRadiuses.big};
+    pointer-events: all;
+
+    .info-box {
+        display: flex;
+        align-items: center;
+        gap: ${(props) => props.theme.margins.medium};
+    }
+`
+
+export default StyledSearchInfo
