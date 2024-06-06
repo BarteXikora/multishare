26af3655f4b6a7e4cc7064dae72a3dd250d60333
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-06 18:15:18 +0200

    Adjusted the FilterWindow styles.

--- src/components/windows/filterWindow/FilterWindow.styles.tsx
+++ src/components/windows/filterWindow/FilterWindow.styles.tsx
@@ -4,13 +4,6 @@ const StyledFilterWindow = styled.div`
     .main button {
         width: 100%;
     }
-
-    .actions {
-        display: flex;
-        justify-content: end;
-        gap: ${(props) => props.theme.margins.medium};
-        margin-top: ${(props) => props.theme.margins.big};
-    }
 `
 
 export default StyledFilterWindow
