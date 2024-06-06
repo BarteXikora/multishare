5f7951e0116832c5add25b43a217a9576a10669e
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-06 11:35:08 +0200

    Created the SearchWindow.

--- src/components/windows/searchWindow/SearchWindow.tsx
+++ src/components/windows/searchWindow/SearchWindow.tsx
@@ -1,7 +1,19 @@
 import StyledSearchWindow from './SearchWindow.styles'
+import Input from '../../ui/input/Input'
+import Button from '../../ui/button/Button'
+
+import iconSearch from '../../../assets/icons/icon-search.svg'
 
 const SearchWindow = () => {
-    return <StyledSearchWindow>search</StyledSearchWindow>
+    return <StyledSearchWindow>
+        <section className="main">
+            <Input placeholder='Szukaj folderów i plików...' />
+
+            <Button $size='big'>
+                <img src={iconSearch} alt='Szukaj' />
+            </Button>
+        </section>
+    </StyledSearchWindow>
 }
 
 export default SearchWindow
