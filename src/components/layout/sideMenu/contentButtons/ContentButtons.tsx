fccc059ce20b9e58853ebb841168fde7143188cc
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-06 14:01:31 +0200

    Fixed functionality of buttons in the SideMenu component so they work with search.

--- src/components/layout/sideMenu/contentButtons/ContentButtons.tsx
+++ src/components/layout/sideMenu/contentButtons/ContentButtons.tsx
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from '../../../../store/store'
 import { useNavigate } from 'react-router-dom'
-import { setFilter } from '../../../../store/features/contentSlice/contentSlice'
+import { setFilter, setSearch } from '../../../../store/features/contentSlice/contentSlice'
 
 import Button from '../../../ui/button/Button'
 
@@ -14,10 +14,12 @@ const ContentButtons = () => {
     const navigate = useNavigate()
 
     const filter = useSelector(state => state.content.filter)
+    const search = useSelector(state => state.content.search)
     const displayType = useSelector(state => state.content.displayType)
 
     const handleClick = (to: 'project' | 'files' | 'trash' | 'star') => {
         dispatch(setFilter({ type: null, time: null, star: null }))
+        dispatch(setSearch(''))
 
         if (to === 'project' && displayType !== 'TREE') navigate('/project')
         if (to === 'files' && displayType !== 'FILES') navigate('/files')
@@ -33,7 +35,7 @@ const ContentButtons = () => {
     return <>
         <Button
             $variant='tertiary'
-            $active={(!filter.star && !filter.time && !filter.type) && displayType === 'TREE'}
+            $active={(!filter.star && !filter.time && !filter.type) && search === '' && displayType === 'TREE'}
             onClick={() => handleClick('project')}
         >
             <img src={iconFolder} alt='Dysk' />
@@ -43,7 +45,7 @@ const ContentButtons = () => {
 
         <Button
             $variant='tertiary'
-            $active={(filter.time || filter.type || displayType === 'FILES') && !filter.star && displayType !== 'TRASH'}
+            $active={(filter.time || filter.type || displayType === 'FILES' || search !== '') && !filter.star && displayType !== 'TRASH'}
             onClick={() => handleClick('files')}
         >
             <img src={iconFiles} alt='Wszystkie pliki' />
