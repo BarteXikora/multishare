6918087612025c258fd45a4e60ed8999e279fe6d
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-06 12:29:57 +0200

    Added functionality to the SearchWindow.

--- src/components/windows/searchWindow/SearchWindow.tsx
+++ src/components/windows/searchWindow/SearchWindow.tsx
@@ -1,3 +1,8 @@
+import { FormEvent, useState } from 'react'
+import { useDispatch } from '../../../store/store'
+import { setSearch } from '../../../store/features/contentSlice/contentSlice'
+import { closeWindow } from '../../../store/features/windowSlice/windowSlice'
+
 import StyledSearchWindow from './SearchWindow.styles'
 import Input from '../../ui/input/Input'
 import Button from '../../ui/button/Button'
@@ -5,14 +10,31 @@ import Button from '../../ui/button/Button'
 import iconSearch from '../../../assets/icons/icon-search.svg'
 
 const SearchWindow = () => {
-    return <StyledSearchWindow>
-        <section className="main">
-            <Input placeholder='Szukaj folderów i plików...' />
+    const dispatch = useDispatch()
 
-            <Button $size='big'>
+    const [searchValue, setSearchValue] = useState<string>('')
+
+    const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if (searchValue.trim() === '') return
+
+        dispatch(setSearch(searchValue.trim()))
+        dispatch(closeWindow())
+    }
+
+    return <StyledSearchWindow>
+        <form className="main" onSubmit={handleSearch}>
+            <Input
+                value={searchValue}
+                onChange={e => setSearchValue(e.target.value)}
+                placeholder='Szukaj folderów i plików...'
+            />
+
+            <Button $size='big' type='submit'>
                 <img src={iconSearch} alt='Szukaj' />
             </Button>
-        </section>
+        </form>
     </StyledSearchWindow>
 }
 
