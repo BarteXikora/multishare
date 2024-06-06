4d6ae422e208f3297f66afc522ba299e62a4350a
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-06 17:45:04 +0200

    Removed the CanNotOpenInTrashWindow styles and used general styles.

--- src/components/windows/canNotOpenInTrashWindow/CanNotOpenInTrashWindow.tsx
+++ src/components/windows/canNotOpenInTrashWindow/CanNotOpenInTrashWindow.tsx
@@ -1,19 +1,16 @@
-import StyledCanNotOpenInTrashWindow from './CanNotOpenInTrashWindow.styles'
 import Button from '../../ui/button/Button'
 
 import iconRestore from '../../../assets/icons/icon-restore.svg'
 
 const CanNotOpenInTrashWindow = () => {
-    return <StyledCanNotOpenInTrashWindow>
-        <section className="info-section">
+    return <>
+        <section>
             <h2>Nie można wyświetlić tego elementu, gdyż znajduje się on w koszu.</h2>
 
-            <p>
-                Aby wyświetlić ten element należy najpierw go przywrócić.
-            </p>
+            <p>Aby wyświetlić ten element należy najpierw go przywrócić.</p>
         </section>
 
-        <section className="action-section">
+        <section className="actions">
             <Button $variant='secondary'>
                 Anuluj
             </Button>
@@ -24,7 +21,7 @@ const CanNotOpenInTrashWindow = () => {
                 Przywróć ten element
             </Button>
         </section>
-    </StyledCanNotOpenInTrashWindow>
+    </>
 }
 
 export default CanNotOpenInTrashWindow
