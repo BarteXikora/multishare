8671efe26f2bc75a1918bd3425d4a7388d95bb2e
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-13 11:33:07 +0200

    Created the addFiles reducer in the uploadListSlice.

--- src/store/features/uploadListSlice/uploadListSlice.ts
+++ src/store/features/uploadListSlice/uploadListSlice.ts
@@ -1,12 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { addUploadFileType, uploadListStateType } from './uploadListSlice.types'
+import { v4 as uuid } from 'uuid'
+
+const initialState: uploadListStateType = []
 
 const uploadListSlice = createSlice({
     name: 'uploadSlice',
-    initialState: [],
+    initialState,
     reducers: {
+
+        addFiles: (state, action: PayloadAction<addUploadFileType[]>) => {
+            const filesToAdd: uploadListStateType = action.payload.map(file => { return { ...file, id: uuid(), status: 'WAITING' } })
+
+            return [...state, ...filesToAdd]
+        },
 
     }
 })
 
 export default uploadListSlice.reducer
-export const { } = uploadListSlice.actions
+export const { addFiles } = uploadListSlice.actions
