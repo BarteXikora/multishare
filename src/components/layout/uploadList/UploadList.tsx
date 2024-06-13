144e32405a6952c7e0873705126db63b65c56384
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-13 21:19:22 +0200

    Added functionality of showing files from the state in the UpdateList component.

--- src/components/layout/uploadList/UploadList.tsx
+++ src/components/layout/uploadList/UploadList.tsx
@@ -1,13 +1,20 @@
+import { useSelector } from '../../../store/store'
+
 import StyledUploadList from './UploadList.styles'
 import Button from '../../ui/button/Button'
 
 import iconUpload from '../../../assets/icons/icon-upload.svg'
 import iconArrow from '../../../assets/icons/icon-arrow-down.svg'
-import iconUploading from '../../../assets/icons/icon-uploading.svg'
-import iconOK from '../../../assets/icons/icon-ok-color.svg'
+import iconWaiting from '../../../assets/icons/icon-waiting.svg'
+import iconUploading from '../../../assets/icons/icon-upload-color.svg'
+import iconDone from '../../../assets/icons/icon-ok-color.svg'
 
 const UploadList = () => {
-    return <StyledUploadList className='_collapsed'>
+    const uploadList = useSelector(state => state.uploadList)
+
+    if (uploadList.length === 0) return null
+
+    return <StyledUploadList>
         <section className="bar">
             <h2>
                 <img src={iconUpload} alt='Przesyłanie plików' />
@@ -21,15 +28,25 @@ const UploadList = () => {
         </section>
 
         <section className="main">
-            <div className="file">
-                <div className='file-name'>Nazwa pliku<span className='extension'>.txt</span></div>
-                <div className='status'><img src={iconOK} alt='Przesłano' /></div>
-            </div>
+            {
+                uploadList.map(file => <div key={file.id} className="file">
+                    <div className='file-name'>
+                        {file.name}<span className='extension'>.{file.extension.toLocaleLowerCase()}</span>
+                    </div>
 
-            <div className="file">
-                <div className='file-name'>Nazwa pliku<span className='extension'>.txt</span></div>
-                <div className='status'><img src={iconUploading} alt='Przesyłanie...' /></div>
-            </div>
+                    <div className='status' title={file.status === 'WAITING' ? 'Oczekuje...' : file.status === 'UPLOADING' ? 'Przesyłanie...' : 'Przesłano'}>
+                        {
+                            file.status === 'WAITING' ?
+                                <img src={iconWaiting} alt='Oczekuje...' />
+                                :
+                                file.status === 'UPLOADING' ?
+                                    <img src={iconUploading} alt='Przesyłanie...' />
+                                    :
+                                    <img src={iconDone} alt='Przesłano' />
+                        }
+                    </div>
+                </div>)
+            }
         </section>
     </StyledUploadList>
 }
