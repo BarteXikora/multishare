f3491a289bb2f0b03176a08041c6c3d7afee95a1
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-17 12:52:29 +0200

    Created the new uploadRequest socketEvent on the mocked server.

--- src/mock/server/socketEvents/uploadRequest.js
+++ src/mock/server/socketEvents/uploadRequest.js
@@ -1,29 +1,63 @@
 const getUserProject = require('../functions/getUserProject')
 const getProjectContent = require('../functions/getProjectContent')
-const setProjestContent = require('../functions/setProjectContent')
+const setProjectContent = require('../functions/setProjectContent')
 
 const uploadRequest = (socket, data) => {
+    const MAX_CONCURRENT_UPLOADS = 3
+    const _MAX_UPLOAD_TIME = 15000
+
     const currentProject = getUserProject(socket)
     const currentContent = getProjectContent(currentProject)
 
-    let uploadedFiles = []
+    let queue = []
+    let activeUploads = 0
 
-    data.data.forEach(f => {
-        uploadedFiles.push({
-            id: Math.floor(Math.random() * 999999999),
-            parentFolder: data.location,
-            name: f.name,
-            extension: f.extension.toUpperCase(),
-            details: {},
-            star: false
-        })
-    })
+    data.forEach(file => queue.push(file))
 
-    currentContent.content.files = [...currentContent.content.files, ...uploadedFiles]
+    const mockedUploadFile = async (file, callback) => {
+        socket.emit('upload_in_progress', file.uploadId)
 
-    setProjestContent(currentProject, currentContent)
+        setTimeout(() => {
+            const newFile = {
+                id: Math.floor(Math.random() * 999999999),
+                parentFolder: file.location,
+                name: file.name,
+                extension: file.extension.toUpperCase(),
+                details: {},
+                star: false
+            }
 
-    socket.to(currentProject).emit('upload_response', uploadedFiles)
+            currentContent.content.files.push(newFile)
+
+            socket.to(currentProject).emit('upload_response', newFile)
+            socket.emit('upload_end', file.uploadId)
+
+            callback()
+
+        }, Math.floor(Math.random() * _MAX_UPLOAD_TIME))
+    }
+
+    const processQueue = () => {
+        const done = () => {
+            activeUploads--
+
+            processQueue()
+
+            if (queue.length === 0 && activeUploads === 0) {
+                setProjectContent(currentProject, currentContent)
+            }
+        }
+
+        while (queue.length > 0 && activeUploads < MAX_CONCURRENT_UPLOADS) {
+            activeUploads++
+
+            const file = queue.shift()
+
+            mockedUploadFile(file, done)
+        }
+    }
+
+    processQueue()
 }
 
 module.exports = uploadRequest
