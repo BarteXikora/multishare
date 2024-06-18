3819cfc26a2af28fc2871d3bb5d9d2b4f457739a
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-18 12:01:59 +0200

    Created better apperance of the MovePill component.

--- src/components/elements/movePill/MovePill.styles.tsx
+++ src/components/elements/movePill/MovePill.styles.tsx
@@ -2,15 +2,20 @@ import styled from 'styled-components'
 
 const StyledMovePill = styled.div`
     position: absolute;
+    display: flex;
+    align-items: center;
+    gap: ${(props) => props.theme.margins.medium};
     background-color: ${(props) => props.theme.colors.white};
     color: ${(props) => props.theme.colors.black};
     padding: ${(props) => props.theme.margins.sectionMedium};
     box-sizing: border-box;
+    white-space: nowrap;
     border-radius: ${(props) => props.theme.borderRadiuses.small};
     border: 2px solid ${(props) => props.theme.colors.black};
     box-shadow: 6px 6px 18px 0 ${(props) => props.theme.colors.black};
     top: 0;
     left: 0;
+    z-index: 100;
 
     h3 {
         margin: 0;
--- src/components/elements/movePill/MovePill.tsx
+++ src/components/elements/movePill/MovePill.tsx
@@ -1,21 +1,76 @@
+import { useState, useEffect } from 'react'
 import { useSelector } from '../../../store/store'
 import useFollowMouse from '../../../functions/useFollowMouse/useFollowMouse'
 
 import StyledMovePill from './MovePill.styles'
+import { elementType } from '../../../store/features/contentSlice/contentSlice.types'
+import getShortenName from '../../../functions/getShortenName/getShortenName'
+
+import iconFolder from '../../../assets/icons/icon-folder-dark.svg'
+import iconFile from '../../../assets/icons/icon-file-dark.svg'
+import iconMultiple from '../../../assets/icons/icon-multiple-elements-dark.svg'
+
+type currentMoveType = {
+    elementsType: elementType | 'MIXED'
+    name: string
+}
 
 const MovePill = () => {
     const elementRef = useFollowMouse()
 
     const onMove = useSelector(state => state.content.onMove)
+    const content = useSelector(state => state.content.currentFolder)
+
+    const [currentMove, setCurrentMove] = useState<currentMoveType>({ elementsType: 'FOLDER', name: '' })
+
+    useEffect(() => {
+        let pillData: currentMoveType = { elementsType: 'FOLDER', name: '' }
+
+        if (onMove.folders.length > 0 && onMove.files.length === 0) {
+            if (onMove.folders.length === 1)
+                pillData.name = content.folders.find(f => f.id === onMove.folders[0])?.name || 'Zaznaczony folder'
+            else
+                pillData.name = `Zaznaczone foldery (${onMove.folders.length})`
+
+        } else if (onMove.folders.length === 0 && onMove.files.length > 0) {
+            pillData.elementsType = 'FILE'
+
+            if (onMove.files.length === 1) {
+                const found = content.files.find(f => f.id === onMove.files[0])
+
+                if (found) pillData.name = found.name + '.' + found.extension.toLocaleLowerCase()
+                else pillData.name = 'Zaznaczony plik'
+
+            } else
+                pillData.name = `Zaznaczone pliki (${onMove.files.length})`
+
+        } else if (onMove.folders.length > 0 && onMove.files.length > 0) {
+            pillData.elementsType = 'MIXED'
+            pillData.name = `Zaznaczone elementy (${(onMove.folders.length + onMove.files.length)})`
+        }
+
+        setCurrentMove(pillData)
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [onMove])
 
     if (onMove.folders.length + onMove.files.length === 0) return null
 
     return <StyledMovePill ref={elementRef}>
-        <h3>
-            {(onMove.folders.length > 0 && onMove.files.length === 0) && 'Foldery: ' + onMove.folders.length}
-            {(onMove.folders.length === 0 && onMove.files.length > 0) && 'Pliki: ' + onMove.files.length}
-            {(onMove.folders.length > 0 && onMove.files.length > 0) && 'Elementy: ' + (onMove.folders.length + onMove.files.length)}
-        </h3>
+        <img
+            src={
+                currentMove.elementsType === 'FOLDER' ?
+                    iconFolder
+                    :
+                    currentMove.elementsType === 'FILE' ?
+                        iconFile
+                        :
+                        iconMultiple
+            }
+            alt='Wybrane elementy'
+        />
+
+        <h3>{getShortenName(currentMove.name, 30)}</h3>
     </StyledMovePill>
 }
 
