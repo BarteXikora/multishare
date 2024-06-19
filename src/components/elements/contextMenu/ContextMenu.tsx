a76d18aa26b0571cd90148a1fa37c2532d8e347c
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-19 09:56:56 +0200

    Used the useClickOutside hook in the ContextMenu component.

--- src/components/elements/contextMenu/ContextMenu.tsx
+++ src/components/elements/contextMenu/ContextMenu.tsx
@@ -1,15 +1,23 @@
-import { useSelector } from '../../../store/store'
+import { useRef } from 'react'
+import { useSelector, useDispatch } from '../../../store/store'
+import useClickOutside from '../../../functions/useClickOutside/useClickOutside'
+import { closeContextMenu } from '../../../store/features/contextMenuSlice/contextMenuSlice'
 
 import StyledContextMenu from './ContextMenu.styles'
 import ElementsContextMenu from './elementsContextMenu/ElementsContextMenu'
 import LocationContextMenu from './locationContextMenu.tsx/LocationContextMenu'
 
 const ContextMenu = () => {
+    const dispatch = useDispatch()
+
     const contextMenu = useSelector(state => state.contextMenu)
+
+    const contextMenuRef = useRef(null)
+    useClickOutside(contextMenuRef, () => dispatch(closeContextMenu()))
 
     if (!contextMenu.isShown) return null
 
-    return <StyledContextMenu>
+    return <StyledContextMenu ref={contextMenuRef}>
         {contextMenu.type === 'ELEMENT' && <ElementsContextMenu />}
 
         {contextMenu.type === 'LOCATION' && <LocationContextMenu />}
