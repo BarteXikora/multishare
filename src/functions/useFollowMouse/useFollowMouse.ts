86e0889f8d3f21e2879682b782c70c2e1b520edd
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-19 10:19:16 +0200

    Moved the getParentPosition function to the separate file.

--- src/functions/useFollowMouse/useFollowMouse.ts
+++ src/functions/useFollowMouse/useFollowMouse.ts
@@ -1,24 +1,5 @@
 import { useEffect, useRef } from 'react'
-
-type coordinatesType = { x: number, y: number }
-
-const getParentPosition = (element: HTMLElement): coordinatesType => {
-    let parent: HTMLElement | null = element.parentElement
-    let result: coordinatesType = { x: 0, y: 0 }
-
-    while (parent) {
-        const style = window.getComputedStyle(parent)
-
-        if (style.position === 'relative') {
-            result.x += parent.offsetLeft
-            result.y += parent.offsetTop
-        }
-
-        parent = parent.parentElement
-    }
-
-    return result
-}
+import getParentPosition, { coordinatesType } from '../getParentPosition/getParentPosition'
 
 const useFollowMouse = () => {
     const elementRef = useRef<HTMLDivElement | null>(null)
