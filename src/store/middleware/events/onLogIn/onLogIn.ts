edede82dd9ae04015f9f63fbf2d87653c034bacf
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-20 14:35:18 +0200

    Used the getDataFromPathname function in the onLogIn middleware.

--- src/store/middleware/events/onLogIn/onLogIn.ts
+++ src/store/middleware/events/onLogIn/onLogIn.ts
@@ -1,8 +1,13 @@
 import socket from '../../../../api/socket'
 import socketEventListeners from '../../socketEventListeners'
+import getDataFromPathname from '../../../../functions/getDataFromPathname/getDataFromPathname'
 
 const onLogIn = (next: any, action: any) => {
-    socket.emit('log_in')
+    if (!('pathname' in action.payload)) return
+
+    let projectId = getDataFromPathname(action.payload.pathname).projectId
+
+    socket.emit('log_in', { projectId })
 
     socket.once('logged_in', (data: any) => next({ ...action, payload: { ...data, status: 'READY' } }))
 
