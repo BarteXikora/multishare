68eaa2d91eef4ff415b963858a6410bb601746e9
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-20 14:34:38 +0200

    Fixed dispatching logIn so it sends pathname.

--- src/components/layout/mainView/MainView.tsx
+++ src/components/layout/mainView/MainView.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useDispatch } from '../../../store/store'
+import { useLocation } from 'react-router-dom'
 import { logIn } from '../../../store/features/userSlice/userSlice'
 import { Outlet } from 'react-router-dom'
 
@@ -10,11 +11,13 @@ import MainBody from '../mainBody/MainBody'
 
 const MainView = () => {
     const dispatch = useDispatch()
+    const location = useLocation()
 
     useEffect(() => {
-        dispatch(logIn({ status: 'LOADING' }))
+        dispatch(logIn({ pathname: location.pathname }))
 
-    }, [dispatch])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return <>
         <SideMenu />
