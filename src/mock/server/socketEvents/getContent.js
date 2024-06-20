0906b5882318aa696da4c455afb46653c53a6562
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-20 13:35:13 +0200

    Removed logging to console from the getContent socket event.

--- src/mock/server/socketEvents/getContent.js
+++ src/mock/server/socketEvents/getContent.js
@@ -2,8 +2,6 @@ const getUserProject = require('../functions/getUserProject')
 const getProjectContent = require('../functions/getProjectContent')
 
 const getContent = (socket) => {
-    console.log('GET CONTENT', getUserProject(socket))
-
     socket.emit('content', getProjectContent(getUserProject(socket)))
 }
 
