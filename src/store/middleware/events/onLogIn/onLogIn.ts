11f13b1dd3d281e03da4b2fce82d505ad849cc91
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-20 13:08:03 +0200

    Fixed the onLogIn middleware.

--- src/store/middleware/events/onLogIn/onLogIn.ts
+++ src/store/middleware/events/onLogIn/onLogIn.ts
@@ -4,7 +4,7 @@ import socketEventListeners from '../../socketEventListeners'
 const onLogIn = (next: any, action: any) => {
     socket.emit('log_in')
 
-    socket.once('logged_in', (data: any) => next({ ...action, payload: data }))
+    socket.once('logged_in', (data: any) => next({ ...action, payload: { ...data, status: 'READY' } }))
 
     socketEventListeners(next)
 }
