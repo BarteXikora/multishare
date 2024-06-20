f1fa245bf3da89088d23824981ad0b7da7733a31
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-20 12:44:41 +0200

    Fixed the getProjectIcon function.

--- src/functions/getProjectIcon/getProjectIcon.ts
+++ src/functions/getProjectIcon/getProjectIcon.ts
@@ -1,4 +1,4 @@
-import { iconType } from '../../store/features/projectSlice/projectSlice.types'
+import { projectIconType } from '../../store/features/userSlice/userSlice.types'
 
 import imgPersonalProject from '../../assets/images/img-personal-project.svg'
 import imgProject from '../../assets/images/img-project.svg'
@@ -8,7 +8,7 @@ type getProjectIconType = {
     content: string,
 }
 
-const getProjectIcon = (icon: iconType | null | undefined): getProjectIconType => {
+const getProjectIcon = (icon: projectIconType | null | undefined): getProjectIconType => {
     if (!icon) return { type: 'ICON', content: imgProject }
     if (icon.type === 'LINK') return { type: 'IMAGE', content: icon.link }
     if (icon.icon === 'PERSONAL_PROJECT') return { type: 'ICON', content: imgPersonalProject }
