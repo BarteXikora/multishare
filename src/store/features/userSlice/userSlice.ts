9863bd9c86afd085b11831986bf7cc1535735e92
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-21 10:32:45 +0200

    Created the setError reducer in the userSLice.

--- src/store/features/userSlice/userSlice.ts
+++ src/store/features/userSlice/userSlice.ts
@@ -3,15 +3,17 @@ import initialState from './initialState'
 
 import logInReducer from './reducers/logIn/logIn'
 import selectProjectReducer from './reducers/selectProject/selectProject'
+import setErrorReducer from './reducers/setError/setError'
 
 export const userSlice = createSlice({
     name: 'userSlice',
     initialState,
     reducers: {
         logIn: logInReducer,
-        selectProject: selectProjectReducer
+        selectProject: selectProjectReducer,
+        setError: setErrorReducer
     }
 })
 
 export default userSlice.reducer
-export const { logIn, selectProject } = userSlice.actions
+export const { logIn, selectProject, setError } = userSlice.actions
