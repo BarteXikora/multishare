bedef85200cec2bda90ed8d8efc6c2ce47acfcd8
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-21 10:33:20 +0200

    Added rendering loadingPage and errorPage to the MainView component.

--- src/components/layout/mainView/MainView.tsx
+++ src/components/layout/mainView/MainView.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { useDispatch } from '../../../store/store'
+import { useSelector, useDispatch } from '../../../store/store'
 import { useLocation } from 'react-router-dom'
 import { logIn } from '../../../store/features/userSlice/userSlice'
 import { Outlet } from 'react-router-dom'
@@ -8,16 +8,23 @@ import SideMenu from '../sideMenu/SideMenu'
 import Window from '../window/Window'
 import Courtain from '../courtain/Courtain'
 import MainBody from '../mainBody/MainBody'
+import ErrorPage from '../../../pages/errorPage/ErrorPage'
+import LoadingPage from '../../../pages/loadingPage/LoadingPage'
 
 const MainView = () => {
     const dispatch = useDispatch()
     const location = useLocation()
+
+    const user = useSelector(state => state.user)
 
     useEffect(() => {
         dispatch(logIn({ pathname: location.pathname }))
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
+    if (user.status === 'LOADING') return <LoadingPage />
+    if (user.status === 'ERROR') return <ErrorPage error={user.message} />
 
     return <>
         <SideMenu />
