c508441d2e788aaf9814653ef7237c4c21cf16f1
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-21 11:23:28 +0200

    Used the responde and respondeError functions in the getContent socket event in the mocked server.

--- src/mock/server/socketEvents/getContent.js
+++ src/mock/server/socketEvents/getContent.js
@@ -1,8 +1,14 @@
 const getUserProject = require('../functions/getUserProject')
 const getProjectContent = require('../functions/getProjectContent')
 
+const { responde, respondeError } = require('../functions/responde')
+
 const getContent = (socket) => {
-    socket.emit('content', getProjectContent(getUserProject(socket)))
+    const content = getProjectContent(getUserProject(socket))
+
+    if (content === null) return respondeError(socket, 'content', 'Nie udało się wczytać zawartości projektu.', true)
+
+    responde(socket, 'content', content)
 }
 
 module.exports = getContent
