cc437d845892cbd7a252e442985a67dafa4920e7
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-23 17:12:30 +0200

    Used the responde and respondeError functions in the getFile socket event in the mocked server.

--- src/store/middleware/functions/beforePreview/beforePreview.ts
+++ src/store/middleware/functions/beforePreview/beforePreview.ts
@@ -2,14 +2,16 @@ import socket from '../../../../api/socket'
 import { resetPreview, setPreview } from '../../../features/previewSlice/previewSlice'
 
 import { Dispatch } from '@reduxjs/toolkit'
+import { setError } from '../../../features/userSlice/userSlice'
 
 const beforePreview = (action: any, next: (action: any) => void, dispatch: Dispatch): boolean => {
     dispatch(resetPreview())
 
     socket.once('file', (data: any) => {
-        if (data === null) return alert('error')
+        if (!('success' in data)) return dispatch(setError('Nie udało się wczytać podglądu pliku.'))
+        if (!data.success) return dispatch(setError(data.message))
 
-        dispatch(setPreview(data))
+        dispatch(setPreview(data.data))
     })
 
     return true
