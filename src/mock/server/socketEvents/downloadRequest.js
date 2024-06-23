b9c6b2f2cac3fc5e2e21e63c45f267bc4be30084
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-23 17:27:18 +0200

    Used the responde and respondeError functions in the downloadRequest socket event in the mocked server.

--- src/mock/server/socketEvents/downloadRequest.js
+++ src/mock/server/socketEvents/downloadRequest.js
@@ -1,5 +1,9 @@
-const downloadRequest = (socket, data) => {
-    socket.emit('download_response', { type: 'RES', data: { data: 'Work in progress...', name: 'Work in progress.txt' } })
+const { responde } = require('../functions/responde')
+
+const downloadRequest = (socket) => {
+    const response = { type: 'RES', data: { data: 'Work in progress...', name: 'Work in progress.txt' } }
+
+    responde(socket, 'download_response', response)
 }
 
 module.exports = downloadRequest
