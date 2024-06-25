3fb0247cafdb704fb8d4a29cd76cfb49e94083b7
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-25 10:41:13 +0200

    Added storaging viewStyle in the local storage.

--- src/functions/useLocalStorage/useLocalStorage.ts
+++ src/functions/useLocalStorage/useLocalStorage.ts
@@ -1,14 +1,30 @@
 import { useEffect } from 'react'
-import { useSelector } from '../../store/store'
+import { useSelector, useDispatch } from '../../store/store'
 import setInLocalStorage from './setInLocalStorage/setInLocalStorage'
+import getFromLocalStorage from './getFromLocalStorage/getFromLocalStorage'
+import { contentViewStyleType } from '../../store/features/viewSlice/initialState.types'
+import { setContentViewStyle } from '../../store/features/viewSlice/viewSlice'
 
 const useLocalStorage = () => {
+    const dispatch = useDispatch()
+
     const user = useSelector(state => state.user)
+    const viewStyle = useSelector(state => state.view.contentViewStyle)
 
     useEffect(() => {
         if (user.status === 'READY') setInLocalStorage('projectId', user.project.selectedProject.id)
 
     }, [user])
+
+    useEffect(() => {
+        const storedViewStyle = getFromLocalStorage<contentViewStyleType>('viewStyle')
+
+        if (storedViewStyle) dispatch(setContentViewStyle(storedViewStyle))
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    useEffect(() => setInLocalStorage('viewStyle', viewStyle), [viewStyle])
 }
 
 export default useLocalStorage
