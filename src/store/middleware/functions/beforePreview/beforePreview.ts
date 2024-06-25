cacccf57edcba51fa105ab92685909d929ec8ca2
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-25 09:57:09 +0200

    Used the setPreviewError in the beforePreview function.

--- src/store/middleware/functions/beforePreview/beforePreview.ts
+++ src/store/middleware/functions/beforePreview/beforePreview.ts
@@ -1,15 +1,17 @@
 import socket from '../../../../api/socket'
-import { resetPreview, setPreview } from '../../../features/previewSlice/previewSlice'
+import { resetPreview, setPreview, setPreviewError } from '../../../features/previewSlice/previewSlice'
 
 import { Dispatch } from '@reduxjs/toolkit'
-import { setError } from '../../../features/userSlice/userSlice'
+import { setMessage } from '../../../features/userSlice/userSlice'
 
 const beforePreview = (action: any, next: (action: any) => void, dispatch: Dispatch): boolean => {
     dispatch(resetPreview())
 
     socket.once('file', (data: any) => {
-        if (!('success' in data)) return dispatch(setError('Nie udało się wczytać podglądu pliku.'))
-        if (!data.success) return dispatch(setError(data.message))
+        if (!('success' in data)) return dispatch(setPreviewError('Nie udało się wczytać podglądu pliku.'))
+        if (!data.success) return dispatch(setPreviewError(data.message))
+
+        if (data.message) dispatch(setMessage(data.message))
 
         dispatch(setPreview(data.data))
     })
