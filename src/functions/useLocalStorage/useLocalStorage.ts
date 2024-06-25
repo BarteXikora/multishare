6a0767ea2c58742163bdf060154a54baa740a9ec
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-25 10:58:38 +0200

    Added storaging filter in the local storage.

--- src/functions/useLocalStorage/useLocalStorage.ts
+++ src/functions/useLocalStorage/useLocalStorage.ts
@@ -4,8 +4,8 @@ import setInLocalStorage from './setInLocalStorage/setInLocalStorage'
 import getFromLocalStorage from './getFromLocalStorage/getFromLocalStorage'
 import { contentViewStyleType } from '../../store/features/viewSlice/initialState.types'
 import { setContentViewStyle } from '../../store/features/viewSlice/viewSlice'
-import { sortType } from '../../store/features/contentSlice/contentSlice.types'
-import { setSort } from '../../store/features/contentSlice/contentSlice'
+import { filterType, sortType } from '../../store/features/contentSlice/contentSlice.types'
+import { setFilter, setSort } from '../../store/features/contentSlice/contentSlice'
 
 const useLocalStorage = () => {
     const dispatch = useDispatch()
@@ -13,6 +13,7 @@ const useLocalStorage = () => {
     const user = useSelector(state => state.user)
     const viewStyle = useSelector(state => state.view.contentViewStyle)
     const sort = useSelector(state => state.content.sort)
+    const filter = useSelector(state => state.content.filter)
 
     useEffect(() => {
         const storedViewStyle = getFromLocalStorage<contentViewStyleType>('viewStyle')
@@ -21,12 +22,16 @@ const useLocalStorage = () => {
         const storedSort = getFromLocalStorage<sortType>('sort')
         if (storedSort) dispatch(setSort(storedSort))
 
+        const storedFilter = getFromLocalStorage<filterType>('filter')
+        if (storedFilter) dispatch(setFilter(storedFilter))
+
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     useEffect(() => { if (user.status === 'READY') setInLocalStorage('projectId', user.project.selectedProject.id) }, [user])
     useEffect(() => setInLocalStorage('viewStyle', viewStyle), [viewStyle])
     useEffect(() => setInLocalStorage('sort', sort), [sort])
+    useEffect(() => setInLocalStorage('filter', filter), [filter])
 }
 
 export default useLocalStorage
