8911207081c6308eb685d1950cadd55f85b4a1d0
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-25 14:19:49 +0200

    Exported components from the Icon component file.

--- src/components/ui/icon/Icon.tsx
+++ src/components/ui/icon/Icon.tsx
@@ -3,17 +3,14 @@ import { ReactSVG } from 'react-svg'
 import StyledIcon from './Icon.styles'
 import { iconType, iconOutlineType } from './Icon.types'
 
-
-const Icon = ({ iconSrc, iconArgs }: { iconSrc: string, iconArgs: iconType }) => {
+export const Icon = ({ iconSrc, iconArgs }: { iconSrc: string, iconArgs: iconType }) => {
     return <StyledIcon $color={iconArgs.$color} $size={iconArgs.$size}><ReactSVG src={iconSrc} /></StyledIcon>
 }
 
-const IconOutline = ({ iconSrc, iconArgs }: { iconSrc: string, iconArgs: iconType }) => {
+export const IconOutline = ({ iconSrc, iconArgs }: { iconSrc: string, iconArgs: iconType }) => {
     return <StyledIcon $color={iconArgs.$color} $size={iconArgs.$size} $outline><ReactSVG src={iconSrc} /></StyledIcon>
 }
 
-const IconOptionOutline = ({ iconSrc, iconArgs }: { iconSrc: string, iconArgs: iconOutlineType }) => {
+export const IconOptionOutline = ({ iconSrc, iconArgs }: { iconSrc: string, iconArgs: iconOutlineType }) => {
     return <StyledIcon $color={iconArgs.$color} $size={iconArgs.$size} $outline={iconArgs.$outline}><ReactSVG src={iconSrc} /></StyledIcon>
 }
-
-export { }
