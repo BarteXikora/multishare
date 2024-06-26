b5af85344c0f19b4aa9e670bbf7db0b3ed48fa83
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-26 15:43:11 +0200

    Created the FilterWarning components animation.

--- src/components/elements/filterWarning/FilterWarning.tsx
+++ src/components/elements/filterWarning/FilterWarning.tsx
@@ -1,27 +1,38 @@
-import { useDispatch } from '../../../store/store'
+import { useSelector, useDispatch } from '../../../store/store'
 import { setFilter } from '../../../store/features/contentSlice/contentSlice'
 
 import StyledFilterWarning from './FilterWarning.styles'
+import AnimatedFilterWarning from './FilterWarning.animation'
 import { IconFilter, IconClose } from '../../ui/icon/Icons'
+
+import { AnimatePresence } from 'framer-motion'
 
 const FilterWarning = () => {
     const dispatch = useDispatch()
+
+    const filter = useSelector(state => state.content.filter)
 
     const handleClearFilters = () => {
         dispatch(setFilter({ time: null, type: null, star: null }))
     }
 
-    return <StyledFilterWarning onClick={handleClearFilters}>
-        <IconFilter $color='dark' />
+    return <AnimatePresence>
+        {
+            (filter.time || filter.type || filter.star) && <AnimatedFilterWarning>
+                <StyledFilterWarning onClick={handleClearFilters}>
+                    <IconFilter $color='dark' />
 
-        <h3>Zastosowano filtry!</h3>
+                    <h3>Zastosowano filtry!</h3>
 
-        <div className="clear-filters">
-            <IconClose />
+                    <div className="clear-filters">
+                        <IconClose />
 
-            <h3>Wyczyść filtry!</h3>
-        </div>
-    </StyledFilterWarning>
+                        <h3>Wyczyść filtry!</h3>
+                    </div>
+                </StyledFilterWarning>
+            </AnimatedFilterWarning>
+        }
+    </AnimatePresence>
 }
 
 export default FilterWarning
