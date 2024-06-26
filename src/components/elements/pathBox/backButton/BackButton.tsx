a5c1baedac797ffd1423d1d62c8b89310c1ef5af
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-26 13:56:51 +0200

    Created the Paths BackButton components animation.

--- src/components/elements/pathBox/backButton/BackButton.tsx
+++ src/components/elements/pathBox/backButton/BackButton.tsx
@@ -1,18 +1,21 @@
 import useGoBack from '../../../../functions/useGoBack/useGoBack'
 
 import StyledBackButton from './BackButton.styles'
+import AnimatedBackButton from './BackButton.animation'
 import { IconBack, IconHome } from '../../../ui/icon/Icons'
 
 const BackButton = ({ isHome }: { isHome: boolean }) => {
     const goBack = useGoBack()
 
     return <StyledBackButton $variant='tertiary' onClick={() => !isHome ? goBack() : null}>
-        {
-            isHome ?
-                <IconHome $outline />
-                :
-                <IconBack />
-        }
+        <AnimatedBackButton key={isHome ? 0 : 1}>
+            {
+                isHome ?
+                    <IconHome $outline />
+                    :
+                    <IconBack />
+            }
+        </AnimatedBackButton>
     </StyledBackButton>
 }
 
