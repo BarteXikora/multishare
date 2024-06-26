28a98c02254444fd0a0cc62fdff7c545ca944901
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-26 15:16:35 +0200

    Usd the useScrenSize hook in the ContentTools animation file.

--- src/components/elements/contentTools/ContentTools.animation.tsx
+++ src/components/elements/contentTools/ContentTools.animation.tsx
@@ -1,40 +1,15 @@
-import { useState, useEffect } from 'react'
 import { ChildrenProps } from '../../../utilities/types'
 import { motion } from 'framer-motion'
-import defaultTheme from '../../../theme/defaultTheme'
-
-type screenSizeType = 'X' | 'D' | 'M'
+import useScreenSize from '../../../functions/useScreenSize/useScreenSize'
 
 const AnimatedContentTools = ({ children }: ChildrenProps) => {
-    const getScreenSize = (): screenSizeType => {
-        const width = window.innerWidth
-        let currentScreenSize: screenSizeType = 'X'
-
-        console.log(Number.parseFloat(defaultTheme.screenBreakpoints.desktop), width)
-
-        if (width <= Number.parseFloat(defaultTheme.screenBreakpoints.mobile)) currentScreenSize = 'M'
-        else if (width <= Number.parseFloat(defaultTheme.screenBreakpoints.desktop)) currentScreenSize = 'D'
-
-        return currentScreenSize
-    }
-
-    const [screenSize, setScreenSize] = useState<screenSizeType>(getScreenSize)
-
-    useEffect(() => {
-        const handleResize = () => setScreenSize(getScreenSize())
-
-        window.addEventListener('resize', handleResize)
-
-        return () => window.removeEventListener('resize', handleResize)
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    const { screenNumberSize } = useScreenSize()
 
     return <motion.div
         className='animated-div'
         initial={{
-            x: screenSize === 'X' ? 5 : screenSize === 'D' ? -5 : 0,
-            y: screenSize === 'M' ? 5 : 0,
+            x: screenNumberSize === 4 ? 5 : screenNumberSize > 1 ? -5 : 0,
+            y: screenNumberSize < 2 ? 5 : 0,
             opacity: 0
         }}
         animate={{ x: 0, y: 0, opacity: 1 }}
