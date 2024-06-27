6a9c0e7637bc55cb3865906d0629551da2aa8550
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-27 15:41:13 +0200

    Created the DetailsSection components show and hie animation.

--- src/components/layout/detailsSection/DetailsSection.tsx
+++ src/components/layout/detailsSection/DetailsSection.tsx
@@ -1,8 +1,11 @@
+import { useEffect } from 'react'
 import { useSelector, useDispatch } from '../../../store/store'
 import { toggle } from '../../../store/features/detailsSectionSlice/detailsSectionSlice'
 import useInvokeDetails from '../../../functions/useInvokeDetails/useInvokeDetails'
+import useScreenSize from '../../../functions/useScreenSize/useScreenSize'
 
 import StyledDetailsSection from './DetailsSection.styles'
+import AnimatedDetailsSction from './DetailsSection.animation'
 import NothingSelectedDetails from './detailsTypes/nothingSelectedDetails/NothingSelectedDetails'
 import SingleFolderDetails from './detailsTypes/singleFolderDetails/SingleFolderDetails'
 import SingleFileDetails from './detailsTypes/singleFileDetails/SingleFileDetails'
@@ -11,6 +14,8 @@ import Button from '../../ui/button/Button'
 import UploadList from '../uploadList/UploadList'
 import { IconClose } from '../../ui/icon/Icons'
 
+import { AnimatePresence } from 'framer-motion'
+
 const DetailsSection = () => {
     const project = useSelector(state => state.user.status === 'READY' ? state.user.project.selectedProject : null)
     const isShown = useSelector(state => state.detailsSection.isShown)
@@ -18,22 +23,42 @@ const DetailsSection = () => {
 
     const dispatch = useDispatch()
 
+    const { screenNumberSize } = useScreenSize()
+
+    useEffect(() => {
+        dispatch(toggle(false))
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [screenNumberSize])
+
     useInvokeDetails()
 
     return <StyledDetailsSection className={`${isShown ? 'shown' : ''}`}>
+        <AnimatePresence>
+            {
+                (screenNumberSize >= 4 || isShown) && <AnimatedDetailsSction>
 
-        <Button className='close-button' $variant='wrong' $size='big' onClick={() => dispatch(toggle(false))}>
-            <IconClose />
-        </Button>
+                    <Button className='close-button' $variant='wrong' $size='big' onClick={() => dispatch(toggle(false))}>
+                        <IconClose />
+                    </Button>
 
-        <section className="main">
-            {content.type === 'EMPTY' && <NothingSelectedDetails project={project} />}
-            {content.type === 'FOLDER' && <SingleFolderDetails content={content} />}
-            {content.type === 'FILE' && <SingleFileDetails content={content} />}
-            {content.type === 'MULTIPLE' && <MultipleDetails content={content} />}
-        </section>
+                    <section className="main">
+                        {content.type === 'EMPTY' && <NothingSelectedDetails project={project} />}
+                        {content.type === 'FOLDER' && <SingleFolderDetails content={content} />}
+                        {content.type === 'FILE' && <SingleFileDetails content={content} />}
+                        {content.type === 'MULTIPLE' && <MultipleDetails content={content} />}
+                    </section>
 
-        <UploadList />
+                    <section className="upload-list">
+                        <UploadList />
+                    </section>
+                </AnimatedDetailsSction>
+            }
+        </AnimatePresence>
+
+        {
+            !(screenNumberSize >= 4 || isShown) && <UploadList />
+        }
     </StyledDetailsSection>
 }
 
