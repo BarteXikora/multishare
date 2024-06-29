9dc10ce005c1bd5eed5f2e3d78b1eef24992f8b1
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-29 18:00:53 +0200

    Fixed the ContextMenu so it works with scroll.

--- src/components/elements/contextMenu/ContextMenu.styles.tsx
+++ src/components/elements/contextMenu/ContextMenu.styles.tsx
@@ -1,14 +1,12 @@
 import styled from 'styled-components'
 
 const StyledContextMenu = styled.div`
-    position: absolute;
+    position: fixed;
     display: flex;
-    /* gap: ${(props) => props.theme.margins.small}; */
     gap: 0;
     flex-direction: column;
     background-color: ${(props) => props.theme.colors.white};
     color: ${(props) => props.theme.colors.black};
-    /* padding: ${(props) => props.theme.margins.sectionMedium}; */
     box-sizing: border-box;
     white-space: nowrap;
     border-radius: ${(props) => props.theme.borderRadiuses.small};
@@ -26,7 +24,6 @@ const StyledContextMenu = styled.div`
 
     hr {
         width: 100%;
-        /* margin: ${(props) => props.theme.margins.medium} 0;  */
     }
 `
 
--- src/functions/useMoveToClick/useMoveToClick.ts
+++ src/functions/useMoveToClick/useMoveToClick.ts
@@ -1,5 +1,4 @@
 import { useEffect } from 'react'
-import getParentPosition, { coordinatesType } from '../getParentPosition/getParentPosition'
 
 const getContainerEnd = (element: HTMLElement): number => {
     let parent: HTMLElement | null = element.parentElement
@@ -19,17 +18,13 @@ const useMoveToClick = (ref: React.RefObject<any>) => {
     const handleClick = (e: MouseEvent) => {
         if (!ref.current) return
 
-        const parentPosition: coordinatesType = getParentPosition(ref.current)
-
         const contextMenuStyles = window.getComputedStyle(ref.current)
 
-        let top = (e.clientY - parentPosition.y)
-        if (top + parentPosition.y + parseFloat(contextMenuStyles.height) >= window.innerHeight)
+        let top = e.clientY
+        if (top + parseFloat(contextMenuStyles.height) >= window.innerHeight)
             top -= parseFloat(contextMenuStyles.height)
 
-        console.log(top, parseFloat(contextMenuStyles.height), top + parseFloat(contextMenuStyles.height), window.innerHeight)
-
-        let left = (e.clientX - parentPosition.x)
+        let left = e.clientX
         if (left + parseFloat(contextMenuStyles.width) >= getContainerEnd(ref.current))
             left -= parseFloat(contextMenuStyles.width)
 
