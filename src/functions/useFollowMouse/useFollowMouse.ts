c078331f149cf0d3e7c70946fa8bde5d3902bfd3
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-29 18:03:27 +0200

    Fixed the MovePill component so it works with scroll.

--- src/functions/useFollowMouse/useFollowMouse.ts
+++ src/functions/useFollowMouse/useFollowMouse.ts
@@ -1,16 +1,13 @@
 import { useEffect, useRef } from 'react'
-import getParentPosition, { coordinatesType } from '../getParentPosition/getParentPosition'
 
 const useFollowMouse = () => {
     const elementRef = useRef<HTMLDivElement | null>(null)
-    const parentOffset = useRef<coordinatesType | null>(null)
 
     const handleMouseMove = (e: MouseEvent) => {
         if (!elementRef.current) return
-        parentOffset.current = getParentPosition(elementRef.current as HTMLElement)
 
-        elementRef.current.style.top = (e.clientY - parentOffset.current.y) + 'px'
-        elementRef.current.style.left = (e.clientX - parentOffset.current.x) + 'px'
+        elementRef.current.style.top = e.clientY + 'px'
+        elementRef.current.style.left = e.clientX + 'px'
     }
 
     useEffect(() => {
