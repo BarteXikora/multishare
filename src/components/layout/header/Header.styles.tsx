2e55262a595f658e41df86b42c2ba58385bfd262
Bartłomiej Sikora <sbartek50@gmail.com>
2024-06-30 10:38:01 +0200

    Fixed the AppPage divs structure, so it is imposible to open ContextMenu in wrong places.

--- src/components/layout/header/Header.styles.tsx
+++ src/components/layout/header/Header.styles.tsx
@@ -7,10 +7,17 @@ const StyledHeader = styled.header`
     background-color: ${(props) => props.theme.colors.white};
     gap: ${(props) => props.theme.margins.medium};
     width: 100%;
+    padding-left: ${(props) => props.theme.margins.extraBig};
+    box-sizing: border-box;
     top: 98px;
     z-index: 1000;
 
+    @media (max-width: ${(props) => props.theme.screenBreakpoints.desktop}) {
+        padding-right: ${(props) => props.theme.margins.extraBig};
+    }
+
     @media (max-width: ${(props) => props.theme.screenBreakpoints.mobile}) {
+        padding: 0 ${(props) => props.theme.margins.medium};
         padding-top: ${(props) => props.theme.margins.small};
         top: 62px;
     }
