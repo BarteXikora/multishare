174a2bc20018231a35625aeb4b757232700364a8
Bartłomiej Sikora <sbartek50@gmail.com>
2024-07-03 13:43:20 +0200

    Added converting the file into base64 in the getFilsToUpload function.

--- src/functions/getFilesToUpload/getFilesToUpload.ts
+++ src/functions/getFilesToUpload/getFilesToUpload.ts
@@ -1,10 +1,29 @@
 import { uploadFileType } from '../../store/features/uploadListSlice/uploadListSlice.types'
 import { v4 as uuid } from 'uuid'
 
-const getFilesToUpload = (files: File[] | null, location: number | null): uploadFileType[] | false => {
+const getFilesToUpload = async (files: File[] | null, location: number | null): Promise<uploadFileType[] | false> => {
     if (!files || !location) return false
 
-    const filesToUpload: uploadFileType[] = files.map(f => {
+    const fileToBase64 = (file: File): Promise<string | false> => {
+        return new Promise(resolve => {
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                if (typeof reader.result === 'string') resolve(reader.result)
+                else resolve(false)
+            }
+
+            reader.onerror = () => resolve(false)
+
+            reader.readAsDataURL(file)
+        })
+    }
+
+    const filesToUpload: (uploadFileType | null)[] = await Promise.all(files.map(async f => {
+        const file = await fileToBase64(f)
+
+        if (!file) return null
+
         return {
             uploadId: uuid(),
             name: f.name.split('.').slice(0, -1).join(''),
@@ -12,11 +31,13 @@ const getFilesToUpload = (files: File[] | null, location: number | null): upload
             parentFolder: location,
             status: 'WAITING',
             uploadPercent: 0,
-            file: new File([f], f.name)
+            file
         }
-    })
+    }))
 
-    return filesToUpload
+    const validFilesToUpload: uploadFileType[] = filesToUpload.filter((f): f is uploadFileType => f !== null)
+
+    return validFilesToUpload
 }
 
 export default getFilesToUpload
