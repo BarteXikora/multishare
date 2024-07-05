46ad0bf4a4e01caff594e1c7c698fccc7c3d3d9e
Bartłomiej Sikora <sbartek50@gmail.com>
2024-07-05 10:28:19 +0200

    Added comments to PreviewContentSection components.

--- src/components/layout/previewContentSection/noPreviewSection/NoPreviewSection.tsx
+++ src/components/layout/previewContentSection/noPreviewSection/NoPreviewSection.tsx
@@ -1,7 +1,16 @@
+/** 
+ * No preview section
+ * 
+ * Rendered in the PreviewContentSection component if opened file do not have a preview. It displays
+ * file type icon.
+**/
+
 import StyledNoPreviewSection from './NoPreviewSection.styles'
 import getPreviewImage from '../../../../functions/fileTypes/getPreviewImage/getPreviewImage'
 
 const NoPreviewSection = ({ extension }: { extension: string }) => {
+
+    // Rendering the component:
     return <StyledNoPreviewSection>
         <div className="icon">
             <img src={getPreviewImage(false, extension)} alt='Podgląd niedostępny' />
--- src/components/layout/previewContentSection/textPreviewSection/TextPreviewSection.tsx
+++ src/components/layout/previewContentSection/textPreviewSection/TextPreviewSection.tsx
@@ -1,8 +1,17 @@
+/** 
+ * Text preview section
+ * 
+ * Rendered in the PrviewContentSection component if opened file is a text file. It displays
+ * the files name and content.
+**/
+
 import { fileType } from '../../../../store/features/contentSlice/contentSlice.types'
 
 import StyledTextPreviewSection from './TextPreviewSection.styles'
 
 const TextPreviewSection = ({ file, data }: { file: fileType, data: string }) => {
+
+    // Rendering the component. 
     return <StyledTextPreviewSection>
         <div className="title">
             <h3>{file.name}</h3>
