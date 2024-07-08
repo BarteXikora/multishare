46ea01856a29c898a11363650d77446bc84e6a57
Bartłomiej Sikora <sbartek50@gmail.com>
2024-07-08 20:08:05 +0200

    Added showing the WorkInProgressWindow to the buttons in the SideMenu component.

--- src/components/layout/sideMenu/projectsButtons/ProjectsButtons.tsx
+++ src/components/layout/sideMenu/projectsButtons/ProjectsButtons.tsx
@@ -6,6 +6,7 @@
 
 import { useDispatch } from '../../../../store/store'
 import { toggle } from '../../../../store/features/sideMenuSlice/sideMenuSlice'
+import { showWindow } from '../../../../store/features/windowSlice/windowSlice'
 
 import Button from '../../../ui/button/Button'
 import { IconAdd, IconProjects } from '../../../ui/icon/Icons'
@@ -13,15 +14,21 @@ import { IconAdd, IconProjects } from '../../../ui/icon/Icons'
 const ProjectsButtons = () => {
     const dispatch = useDispatch()
 
+    // Handling click on the buttons:
+    const __handleClick = () => {
+        dispatch(showWindow('WIP'))
+        dispatch(toggle(false))
+    }
+
     // Rendering the component:
     return <>
-        <Button $variant='tertiary' onClick={() => dispatch(toggle(false))}>
+        <Button $variant='tertiary' onClick={__handleClick}>
             <IconAdd />
 
             Utwórz projekt
         </Button>
 
-        <Button $variant='tertiary' $active onClick={() => dispatch(toggle(false))}>
+        <Button $variant='tertiary' $active onClick={__handleClick}>
             <IconProjects />
 
             Dostępne projekty
