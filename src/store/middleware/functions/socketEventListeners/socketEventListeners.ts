b8c6087f63a00e13104cf1e08954ea1251608d20
Bartłomiej Sikora <sbartek50@gmail.com>
2024-07-08 19:03:48 +0200

    Added comments to the socketEventListeners function.

--- src/store/middleware/functions/socketEventListeners/socketEventListeners.ts
+++ src/store/middleware/functions/socketEventListeners/socketEventListeners.ts
@@ -1,3 +1,25 @@
+/**
+ * socketEventListeners
+ * 
+ * This function manages socket respondes from the server. It is called in the beforeLogIn function.
+ * It creates socket event listeners.
+ * 
+ * The events array contain object with events names and corresponding redux action names. It can also
+ * attribute getPayload and callback functions.
+ * 
+ * Logic in order:
+ * 
+ * - the function creates socket events for all records in the events array. 
+ * - it sets the action name and handleEvent function. 
+ * - the handleEvent function first check if there is a callback function set. If there
+ *      is it calls it and returns.
+ * - if there is no callback function it checks if data recived from the server contains the 'success'
+ *      value. If it doesn't it dispatches an error. It there is 'success' value, but set to false it
+ *      also dispatches error with message from the server.
+ * - if server responded with 'success' set to true, the handleEvent function calls next() with foud in the
+ *      events array action name. It also sets messages from the server, if there are any.
+ */
+
 import socket from '../../../../api/socket'
 import { setError, setMessage } from '../../../features/userSlice/userSlice'
 
@@ -6,6 +28,7 @@ import handleDisconnect from '../handleDisconnect/handleDisconnect'
 
 import { Dispatch } from '@reduxjs/toolkit'
 
+// Event array type:
 type eventsType = {
     event: string
     actionType: string | null
@@ -13,6 +36,7 @@ type eventsType = {
     callback?: (data: any, dispatch: Dispatch) => void
 }[]
 
+// The events array
 const events: eventsType = [
     { event: 'content', actionType: null, callback: handleLoadContent },
     { event: 'selected_project', actionType: 'userSlice/selectProject' },
@@ -30,9 +54,14 @@ const events: eventsType = [
 ]
 
 const socketEventListeners = (next: any, dispatch: Dispatch) => {
+
+    // Handling the event:
     const handleEvent = (actonType: string | null, data: any, getPayload?: (data: any) => any, callback?: (data: any, dispatch: Dispatch) => void) => {
+
+        // Calling the callback function if found and returning:
         if (callback) return callback(data.data, dispatch)
 
+        // Handling errors from the server:
         if (!('success' in data)) return dispatch(setError('Wystąpił błąd.'))
         if (!data.success) {
             if (data.fatal) return dispatch(setError(data.message))
@@ -40,11 +69,14 @@ const socketEventListeners = (next: any, dispatch: Dispatch) => {
             dispatch(setMessage(data.message))
         }
 
+        // Setting messages from the server if there are any:
         if (data.message) dispatch(setMessage(data.message))
 
+        // Calling the next:
         if (actonType !== null) next({ type: actonType, payload: getPayload ? getPayload(data.data) : data.data })
     }
 
+    // Creating events based on the events array:
     events.forEach(event => {
         socket.on(event.event, (data: any) => handleEvent(event.actionType, data, event.getPayload, event.callback))
     })
