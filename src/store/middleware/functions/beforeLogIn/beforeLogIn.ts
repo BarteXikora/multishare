3a21369f070e742f9c94a3a3d8ef1210cc90a0a2
Bartłomiej Sikora <sbartek50@gmail.com>
2024-07-08 19:27:04 +0200

    Added comments to the socketMiddleware functions.

--- src/store/middleware/functions/beforeLogIn/beforeLogIn.ts
+++ src/store/middleware/functions/beforeLogIn/beforeLogIn.ts
@@ -1,3 +1,12 @@
+/**
+ * beforeLogIn function
+ * 
+ * This function is called in the socketEvents function if log in action takes place. It returns boolean
+ * whitch determinates if the socketEvents function will continue.
+ * 
+ * It creates the logged_in socket event listener. It also calls socketEventListeners function. 
+ */
+
 import socket from '../../../../api/socket'
 import { setError, setMessage } from '../../../features/userSlice/userSlice'
 import socketEventListeners from '../socketEventListeners/socketEventListeners'
@@ -5,20 +14,28 @@ import socketEventListeners from '../socketEventListeners/socketEventListeners'
 import { Dispatch } from '@reduxjs/toolkit'
 
 const beforeLogIn = (action: any, next: (action: any) => void, dispatch: Dispatch): boolean => {
+
+    // Returning false if payload doesn't contain the pathname value:
     if (!('pathname' in action.payload)) return false
 
+    // Creating the socket event listener for logged_in action:
     socket.once('logged_in', (data: any) => {
+
+        // Dispatching an error if server respondes with one:
         if (!('success' in data)) return dispatch(setError('Wystąpił błąd.'))
         if (!data.success) {
             if (data.fatal) return dispatch(setError(data.message))
             return dispatch(setMessage(data.message))
         }
 
+        // Setting messages if sent from the server:
         if (data.message) dispatch(setMessage(data.message))
 
+        // Calling the next():
         next({ ...action, payload: { ...data.data, message: data.message } })
     })
 
+    // Calling the socketEventListeners function:
     socketEventListeners(next, dispatch)
 
     return true
--- src/store/middleware/functions/getLogInActon/getLogInAction.ts
+++ src/store/middleware/functions/getLogInActon/getLogInAction.ts
@@ -1,10 +1,23 @@
+/**
+ * getLogInAction function
+ * 
+ * The function is called in the socketEvents function to get data for the log_in socket event.
+ * It gets project Id from the pathname. If pathname doesn't contain project ID, it gets it 
+ * from the local storage.
+ */
+
 import getDataFromPathname from '../../../../functions/getDataFromPathname/getDataFromPathname'
 import getFromLocalStorage from '../../../../hooks/useLocalStorage/getFromLocalStorage/getFromLocalStorage'
 
 const getLogInAction = (action: any) => {
+
+    // Getting project ID from the pathname: 
     let projectId = getDataFromPathname(action.payload.pathname).projectId
+
+    // Getting project ID from the local storage if not found in the pathname:
     if (!projectId) projectId = getFromLocalStorage<number>('projectId')
 
+    // Returning the found value:
     return { projectId }
 }
 
