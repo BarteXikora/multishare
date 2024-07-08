467f7670482175b9409ae6e235e84c6084832d67
Bartłomiej Sikora <sbartek50@gmail.com>
2024-07-08 20:01:35 +0200

    Prepared the WorkInProgress window.

---
+++ src/components/windows/workInProgressWindow/WorkInProgressWindow.tsx
@@ -0,0 +1,12 @@
+/**
+ * WorkInProgress window
+ * 
+ * This window is shown when user attempts to do something, but the functionality it not
+ * ready yet.
+ */
+
+const WorkInProgressWindow = () => {
+    return <></>
+}
+
+export default WorkInProgressWindow
