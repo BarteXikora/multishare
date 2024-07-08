636968736d05923a9f02a32535b63295d6b6a41b
Bartłomiej Sikora <sbartek50@gmail.com>
2024-07-08 17:44:16 +0200

    Added comments to the previewSlice and its reducers.

--- src/store/features/previewSlice/reducers/initializePreview/initializePreview.ts
+++ src/store/features/previewSlice/reducers/initializePreview/initializePreview.ts
@@ -1,7 +1,15 @@
+/**
+ * initializePreview reducer of the redux previewSlice
+ * 
+ * It only sets status to loading, the functionality is placed in the middleware.
+ */
+
 import { PayloadAction } from '@reduxjs/toolkit'
 import { previewStateType } from '../../initialState.types'
 
 const initializePreview = (state: previewStateType, action: PayloadAction<number>) => {
+
+    // Setting the new state:
     state.content.status = 'LOADING'
 }
 
--- src/store/features/previewSlice/reducers/resetPreview/resetPreview.ts
+++ src/store/features/previewSlice/reducers/resetPreview/resetPreview.ts
@@ -1,7 +1,15 @@
+/**
+ * resetPreview reducer of the redux previewSlice
+ * 
+ * It resets the preview by setting status to loading.
+ */
+
 import { PayloadAction } from '@reduxjs/toolkit'
 import { previewStateType } from '../../initialState.types'
 
 const resetPreview = (state: previewStateType, action: PayloadAction) => {
+
+    // Setting the new state:
     state.content = { status: 'LOADING' }
 }
 
